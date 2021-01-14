c41b7372da5c6927c7c2ea943ad88024ca4d6533
Joachim Heck <joachim@heckcorp.org>
2021-01-13 18:10:24 -0700

    Fixed a bug (the same one everyone else had but I couldn't even understand the explanations, let alone the problem statement), and switched to tail recursion. Problem 22 solved.

--- advent-22/src/advent_22/core.clj
+++ advent-22/src/advent_22/core.clj
@@ -33,14 +33,15 @@
 
 
 (defn score-deck [deck]
-  (map-indexed (list %1 %2) (reverse deck)))
+  (map-indexed #(list %1 %2) (reverse deck)))
 
 (defn play-and-score [init-state game]
   (let [players (map first init-state)
-        winner (apply game (map second init-state))]
-    (str (nth players (first winner))
+        result (apply game (map second init-state))]
+    (println result)
+    (str (nth players (first result))
          " wins with "
-         (->> winner
+         (->> result
               second
               reverse
               (map-indexed #(* (inc %1) %2))
@@ -58,33 +59,36 @@
 (defn recursive-combat
   "Returns a list of the winning player and the winner's deck"
   ([d1 d2] (recursive-combat d1 d2 #{} 0 0))
-  ([d1 d2 game] (recursive-combat d1 d2 #{} game 0))
-  ([d1 d2 prev game round]
-   (println "game" game "round" round)
-   (cond (some #{(list d1 d2)} prev) (list 0 d1)
-         (empty? d1) (list 1 d2)
-         (empty? d2) (list 0 d1)
+  ([d1 d2 history game round]
+;;   (println "game" game "round" round)
+   (cond (empty? d1) (list 1 d2 :card-exhaustion)
+         (empty? d2) (list 0 d1 :card-exhaustion)
+         (some #{(list d1 d2)} history) (list 0 d1 :infinite-recursion)
          :else
          (let [c1 (first d1)
                c2 (first d2)]
            (let [winner
-                 (if (and (>= (count (subvec d1 1)) c1)
-                          (>= (count (subvec d2 1)) c2))
-                   (first (recursive-combat (subvec d1 1) (subvec d2 1) (inc game)))
+                 (if (and (> (count d1) c1) (> (count d2) c2))
+                   (let [result (recursive-combat (subvec d1 1 (inc c1)) (subvec d2 1 (inc c2)) #{} (inc game) 0)]
+                     ;; (println "Game" (inc game) "ended by" (last result))
+                     (first result))
                    (if (> c1 c2) 0 1))]
              (if (= winner 0)
-               (recursive-combat (conj (subvec d1 1) c1 c2)
-                                 (subvec d2 1)
-                                 (conj prev (list d1 d2))
-                                 game (inc round))
-               (recursive-combat (subvec d1 1)
-                                 (conj (subvec d2 1) c2 c1)
-                                 (conj prev (list d1 d2))
-                                 game (inc round))))))))
+               (recur (conj (subvec d1 1) c1 c2)
+                      (subvec d2 1)
+                      (conj history (list d1 d2))
+                      game (inc round))
+               (recur (subvec d1 1)
+                      (conj (subvec d2 1) c2 c1)
+                      (conj history (list d1 d2))
+                      game (inc round))))))))
 
 
 ;; (play-and-score (read-input small-input) recursive-combat)
 ;; => "Player 2 wins with 291"
 
-(play-and-score (read-input small-input-2) recursive-combat)
-(play-and-score (read-input large-input) recursive-combat)
+;(play-and-score (read-input small-input-2) recursive-combat)
+
+;(play-and-score (read-input large-input) recursive-combat)
+;;(0 [46 6 42 8 33 20 50 27 40 23 49 21 36 9 43 25 31 19 48 39 45 29 22 16 12 2 1 4 47 38 35 17 44 37 32 3 41 34 30 18 26 10 28 24 15 11 14 7 13 5] :card-exhaustion)
+;; "Player 1 wins with 35836"
