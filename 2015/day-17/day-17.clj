8fe35960176e0012432c21048055824a985d50ed
Joachim Heck <joachim@heckcorp.org>
2021-01-20 17:10:25 -0700

    Solutions to AOC 2015, day 17: divide egg nog among containers. Once I got part 1, part 2 was trivial.

--- 2015/day-17/day-17.clj
+++ 2015/day-17/day-17.clj
@@ -11,31 +11,54 @@
 
 (def test-containers [20 15 10 5 5])
 
-;; [20 15 10 5 5]
-;; separate first element
-;; 20 [15 10 5 5]
-;;     Check > target? I don't think I need to
-;; Remaining amount = target - first = 5
-;; Recurse for each set remaining after dropping the first item
+(def real-amount 150)
 
+(def real-containers [50 44 11 49 42 46 18 32 26 40 21 7 18 43 10 47 36 24 22 40])
+
+;; [20 15 10 5 5] 25
+;; separate first element
+;; 20 [15 10 5 5] 25
+;; Now we need two paths
+;; 1. Find the numbers in the rest of the vector that sum to 25-20 = 5
+;;    Return 20 + each of those sets
+;;    map (list 20 %) (recurse [15 10 5 5] 5)
+;;    15 [10 5 5] 5 - 15 > 5 so don't do part 1
+;; 2. Find the numbers in the rest of the vector that sum to 25
+;;    (recurse [15 10 5 5] 25)
 
 (defn group-containers [[container & rest-c :as containers] amount]
-  (if (<= amount 0) '() 
-      (concat
-       (list amount)
-       (if (<= container amount)
-         (for [i (range (count rest-c))]
-           (group-containers (drop i rest-c) (- amount container))
-           )
-         '())
-       (for [i (range (count rest-c))]
-         (group-containers (drop i rest-c) amount)
-         )
-       ))
-  )
+  (if (= container amount) (concat (list amount) (group-containers rest-c amount))
+      (if (or (nil? containers) (empty? containers)) '()
+          (concat
+           (if (< amount container) '()
+               (map #(flatten (list container %)) (group-containers rest-c (- amount container))))
+           (group-containers rest-c amount)))))
 
-(group-containers test-containers test-amount)
+;; (time (count (group-containers containers test-amount)))
+;; => ((20 5) (20 5) (15 10) (15 (5 5)))
+;; "Elapsed time: 0.1705 msecs"
 
+;; (time (count (group-containers (reverse (sort real-containers)) real-amount)))
+;; => 654
+;; "Elapsed time: 5.5469 msecs"
 
 
-(filter #(= test-amount (apply + %)) (group-containers test-containers test-amount))
+
+;; Part 2
+;; How many ways are there to use the minimum number of containers?
+(-> real-containers
+    sort
+    reverse
+    (group-containers real-amount)
+    (->>
+     (group-by count)
+     first ;; count + items
+     second ;; items
+     count))
+
+;; (time (count (second (first 
+;;                       (group-by count
+;;                                 (group-containers (reverse (sort real-containers)) real-amount))))))
+;; => 57
+;; "Elapsed time: 20.0843 msecs"
+
