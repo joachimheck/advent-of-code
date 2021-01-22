13ab929154e722fec5b9035d1f70d0a7a4b10cb0
Joachim Heck <joachim@heckcorp.org>
2021-01-21 18:32:54 -0700

    Got the solution to part 1. Was thinking I needed to do part 2 backwards, but maybe not.

--- 2015/day-19/day-19.clj
+++ 2015/day-19/day-19.clj
@@ -7,39 +7,53 @@
 ;; Part 1
 ;; Count distinct variants of single replacements to a molecule.
 
-(def test-input {:molecule "HOH" :transforms {"H" '("HO" "OH") "O" '("HH")}})
-(def test-input-2 {:molecule "SieMgH" :transforms {"H" '("HO" "OH") "Si" '("Ti") "e" '("P")}})
+(defn parse-input [lines molecule]
+  (let [[trxs inv-trxs]
+        (reduce
+         (fn [[trxs inv-trxs] line]
+           (let [[_ from to] (re-matches #"(\w+) => (\w+)" line)]
+             (list
+              (merge-with concat trxs (assoc {} from (list to)))
+              (merge-with concat inv-trxs (assoc {} to (list from))))
+             ))
+         '({} {})
+         lines)]
+    (assoc {}
+           :molecule molecule
+           :transforms trxs
+           :inv-trans inv-trxs)))
+
+(def test-input
+  (parse-input '("e => H" "e => O" "H => HO" "H => OH" "O => HH") "HOH"))
+
+(def test-input-2
+  (parse-input '("H => HO" "H => OH" "Si => Ti" "e => P") "SieMgH"))
 
 (def real-input
- (let [[part1 molecule] (str/split (slurp "input-19.txt") #"\R\R")
-       lines (str/split-lines part1)]
-   (assoc {}
-          :molecule molecule
-          :transforms
-          (reduce
-           (fn [acc line]
-             (let [[_ from to] (re-matches #"(\w+) => (\w+)" line)]
-               (merge-with concat acc (assoc {} from (list to)))
-               ))
-           {}
-           lines))))
+  (let [[part1 molecule] (str/split (slurp "input-19.txt") #"\R\R")
+        lines (str/split-lines part1)]
+    (parse-input lines (str/replace molecule #"\R" ""))))
+
 
 (defn match [alphabet chars]
   (cond (alphabet (str/join chars)) (str/join chars)
         (alphabet (str (first chars))) (str (first chars))
         :else (first chars)))
 
-(split-molecule test-input-2)
-
 (defn split-molecule [{molecule :molecule transforms :transforms}]
-  (let [alphabet (set (keys transforms))]
-    (vec
-     (for [cc (partition 2 1 (str/join (list molecule "0")))
-           :let [match (match alphabet cc)]
-           :when match]
-       match))))
+;;  (println molecule (type molecule))
+  (if (<= (count molecule) 1) (list molecule)
+      (vec
+       (let [alphabet (set (keys transforms))
+             sub1 (subs molecule 0 1)
+             sub2 (subs molecule 0 2)
+             match1 (alphabet sub1)
+             match2 (alphabet sub2)]
+         (if match2
+           (concat (list sub2) (split-molecule {:molecule (subs molecule 2) :transforms transforms}))
+           (concat (list sub1) (split-molecule {:molecule (subs molecule 1) :transforms transforms})))))))
 
-(defn replace [{transforms :transforms :as input}]
+(defn replace-atoms [{transforms :transforms :as input}]
   (distinct
    (flatten
     (let [molecule (split-molecule input)]
@@ -48,19 +62,47 @@
               atom (get molecule i)
               post (subvec molecule (inc i))]
           (for [t (get transforms atom)]
-            (str/join (concat pre t post))))))
-    )))
+            (str/join (concat pre t post)))))))))
 
-;; (replace test-input)
+;; (replace-atoms test-input)
 ;; => ("HOOH" "OHOH" "HHHH" "HOHO")
-;; (count (replace test-input))
+;; (count (replace-atoms test-input))
 ;; => 4
 
-;; (count (replace real-input))
+;; (count (replace-atoms real-input))
 ;; => 189 too low! - I'm assuming one character per atom.
 
-;; (count (replace real-input))
+;; (count (replace-atoms real-input))
 ;; => 507 too low, again! I'm dropping atoms that aren't transformable, that's probably it.
 
-;; (count (replace real-input))
+;; (count (replace-atoms real-input))
 ;; => 585 too high? Maybe I'm not correctly including the extra characters.
+
+;; (count (replace-atoms real-input))
+;; => 509
+
+
+
+;; Part 2
+(defn reduce-molecule [{molecule :molecule txs :transforms inv-txs :inv-trans :as input} limit]
+  (let [inv-keys (reverse (sort-by count (keys inv-txs)))
+        ]
+    (loop [i 0
+           mol molecule]
+      (if (< limit i)
+        mol
+        (recur (inc i)
+               (reduce
+                (fn [mol k]
+;;                  (println i "key" k mol)
+                  (str/replace mol k (first (get inv-txs k))))
+                molecule
+                inv-keys))
+        ))))
+
+
+
+(reduce-molecule test-input 100)
+;; => "CRnSiRnBCaPMgArCaSiRnTiMgArSiRnMgArCaPRnPMgArSiRnCaCaFYSiRnFYFArCaPMgArPRnFArCaSiRnFYCaCaRnFArArSiRnPMgYCaCaFArSiRnPMgArBCaCaCaCaCaCaCaSiRnFArRnFArCaCaSiRnCaFArMgArThSiRnTiRnFArFArCaCaSiRnMgArPRnFArCaRnPMgAr"
+;; => "CRnSiRnBCaPMgArCaSiRnTiMgArSiRnMgArCaPRnPMgArSiRnCaCaFYSiRnFYFArCaPMgArPRnFArCaSiRnFYCaCaRnFArArSiRnPMgYCaCaFArSiRnPMgArBCaCaCaCaCaCaCaSiRnFArRnFArCaCaSiRnCaFArMgArThSiRnTiRnFArFArCaCaSiRnMgArPRnFArCaRnPMgAr"
+;; => "CRnSiRnBCaPMgArCaSiRnTiMgArSiRnMgArCaPRnPMgArSiRnCaCaFYSiRnFYFArCaPMgArPRnFArCaSiRnFYCaCaRnFArArSiRnPMgYCaCaFArSiRnPMgArBCaCaCaCaCaCaCaSiRnFArRnFArCaCaSiRnCaFArMgArThSiRnTiRnFArFArCaCaSiRnMgArPRnFArCaRnPMgAr"
