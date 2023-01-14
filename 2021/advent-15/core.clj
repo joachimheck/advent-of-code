e439f6d9a50611f4a84124adf7370555707d80b9
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-01-14 10:01:16 -0700

    Solution for AoC 2021 day 15, part 2.

--- 2021/advent-15/core.clj
+++ 2021/advent-15/core.clj
@@ -62,7 +62,7 @@
               ;; (println "picking" current "from open-set" (sort-by second (map #(list % (get f-scores %)) open-set)))
               (if (= current goal)
                 (find-path-in-froms froms start goal)
-                (let [open-set (into #{} (remove #{current} open-set))
+                (let [open-set (disj open-set current)
                       neighbors (neighbors current grid)]
                   (let [{new-open-set :open-set new-froms :froms new-f-scores :f-scores new-g-scores :g-scores :as reduce-result}
                         (reduce (fn [result neighbor]
@@ -96,3 +96,170 @@
 
 
 
+;; Part 2
+;; Expand the map four times in each direction (into a 5x5 grid), increasing all values by one (mod 10) with each step.
+(defn inc-mod10 [x]
+  (if (< x 9)
+    (inc x)
+    1))
+
+(defn inc-line [l]
+  (mapv inc-mod10 l))
+
+(defn widen-line [line]
+  (vec (apply concat (take 5 (iterate inc-line line)))))
+
+(defn expand-grid [grid]
+ (let [widened-grid (mapv widen-line grid)
+       deepened-lines (map (fn [line] (take 5 (iterate inc-line line))) widened-grid)]
+   (vec (apply concat (apply map vector deepened-lines)))))
+
+(defn compute-risk-expanded [f]
+  (let [grid (parse-input f)
+        expanded-grid (expand-grid grid)
+        path (find-path expanded-grid)]
+    (->> path
+         rest ;; Don't count the starting position's risk.
+         (map (fn [pos] (get-point expanded-grid pos)))
+         (apply +))))
+
+
+;; (time (compute-risk-expanded small-input))
+;; "Elapsed time: 346.6713 msecs"
+;; 315
+
+;; (time (compute-risk-expanded large-input))
+;; "Elapsed time: 423371.5081 msecs"
+;; 2851
+
+
+;; It works, but it's slow. Can I speed it up with transient collections?
+(defn find-path-transient [grid]
+  (let [width (count (first grid))
+        height (count grid)
+        start [0 0]
+        goal [(dec width) (dec height)]]
+    (loop [open-set #{start}
+           froms (transient {})
+           f-scores (transient {start (manhattan-distance start goal)})
+           g-scores (transient {start 0})]
+      (cond (zero? (count open-set))
+            :failure-no-open-nodes
+            :else
+            (let [current (first (first (sort-by second (map #(list % (get f-scores %)) open-set))))]
+              ;; (println "picking" current "from open-set" (sort-by second (map #(list % (get f-scores %)) open-set)))
+              (if (= current goal)
+                (find-path-in-froms (persistent! froms) start goal)
+                (let [open-set (disj open-set current)
+                      neighbors (neighbors current grid)]
+                  (let [{new-open-set :open-set new-froms :froms new-f-scores :f-scores new-g-scores :g-scores}
+                        (loop [neighbors neighbors
+                               froms froms
+                               f-scores f-scores
+                               g-scores g-scores
+                               open-set open-set]
+                          (if (empty? neighbors)
+                            {:froms froms :g-scores g-scores :f-scores f-scores :open-set open-set}
+                            (let [neighbor (first neighbors)
+                                  tentative-g-score (+ (get g-scores current) (get-point grid neighbor))]
+                              ;;(println "g-score" [1 1] (get g-scores [1 1]))
+                              ;; (if (< tentative-g-score (get g-scores neighbor Integer/MAX_VALUE))
+                              ;;   (println "updating g-score " current "->" neighbor "from" (get g-scores neighbor) "to" tentative-g-score))
+                              (if (< tentative-g-score (get g-scores neighbor Integer/MAX_VALUE))
+                                (recur
+                                 (remove #{neighbor} neighbors)
+                                 (assoc! froms neighbor current)
+                                 (assoc! f-scores neighbor (+ tentative-g-score (manhattan-distance neighbor goal)))
+                                 (assoc! g-scores neighbor tentative-g-score)
+                                 (conj open-set neighbor))
+                                (recur (remove #{neighbor} neighbors) froms f-scores g-scores open-set)))))]
+                    (recur new-open-set new-froms new-f-scores new-g-scores)))))))))
+
+(defn compute-risk-expanded-transient [f]
+  (let [grid (parse-input f)
+        expanded-grid (expand-grid grid)
+        path (find-path-transient expanded-grid)]
+    ;; (println "path" path)
+    (if (not (seq? path))
+      path
+      (->> path
+           rest ;; Don't count the starting position's risk.
+           (map (fn [pos] (get-point expanded-grid pos)))
+           (apply +)
+           ))))
+
+;; This is faster, but not much.
+;; (time (compute-risk-expanded-transient small-input))
+;; "Elapsed time: 265.2823 msecs"
+;; 315
+
+;; (time (compute-risk-expanded-transient large-input))
+;; "Elapsed time: 349066.9539 msecs"
+;; 2851
+
+
+;; What about using a sorted set for open-set?
+(defn compare-seconds [x y]
+  (let [c (compare (second x) (second y))]
+    (if (not= c 0)
+      c
+      (compare (first x) (first y)))))
+
+(defn find-path-transient-sorted [grid]
+  (let [width (count (first grid))
+        height (count grid)
+        start [0 0]
+        goal [(dec width) (dec height)]]
+    (loop [open-set (sorted-set-by compare-seconds (list start 0))
+           froms (transient {})
+           f-scores (transient {start (manhattan-distance start goal)})
+           g-scores (transient {start 0})]
+      (cond (zero? (count open-set))
+            :failure-no-open-nodes
+            :else
+            (let [[current _ :as current-pair] (first open-set)]
+              (if (= current goal)
+                (find-path-in-froms (persistent! froms) start goal)
+                (let [open-set (disj open-set current-pair)
+                      neighbors (neighbors current grid)]
+                  (let [{new-open-set :open-set new-froms :froms new-f-scores :f-scores new-g-scores :g-scores}
+                        (loop [neighbors neighbors
+                               froms froms
+                               f-scores f-scores
+                               g-scores g-scores
+                               open-set open-set]
+                          (if (> (count open-set) 20) :too-big-open-set)
+                          (if (empty? neighbors)
+                            {:froms froms :g-scores g-scores :f-scores f-scores :open-set open-set}
+                            (let [neighbor (first neighbors)
+                                  tentative-g-score (+ (get g-scores current) (get-point grid neighbor))]
+                              (if (< tentative-g-score (get g-scores neighbor Integer/MAX_VALUE))
+                                (recur
+                                 (remove #{neighbor} neighbors)
+                                 (assoc! froms neighbor current)
+                                 (assoc! f-scores neighbor (+ tentative-g-score (manhattan-distance neighbor goal)))
+                                 (assoc! g-scores neighbor tentative-g-score)
+                                 (conj open-set (list neighbor (+ tentative-g-score (manhattan-distance neighbor goal)))))
+                                (recur (remove #{neighbor} neighbors) froms f-scores g-scores open-set)))))]
+                    (recur new-open-set new-froms new-f-scores new-g-scores)))))))))
+
+(defn compute-risk-expanded-transient-sorted [f]
+  (let [grid (parse-input f)
+        expanded-grid (expand-grid grid)
+        path (find-path-transient-sorted expanded-grid)]
+    (if (not (seq? path))
+      path
+      (->> path
+           rest ;; Don't count the starting position's risk.
+           (map (fn [pos] (get-point expanded-grid pos)))
+           (apply +)
+           ))))
+
+
+;; (time (compute-risk-expanded-transient-sorted small-input))
+;; "Elapsed time: 71.6524 msecs"
+;; 315
+
+;; (time (compute-risk-expanded-transient-sorted large-input))
+;; "Elapsed time: 8143.6049 msecs"
+;; 2851
