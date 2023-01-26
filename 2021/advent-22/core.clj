e2e34ac80e3a6c7a905d01b4b8a13f25bf876c75
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-01-26 07:25:09 -0700

    Solution for AoC 2021, day 22.

--- 2021/advent-22/core.clj
+++ 2021/advent-22/core.clj
@@ -98,6 +98,7 @@
   ;; bx1 ax1 bx2 ax2 => overlap: [ax1 bx2] other: ([(inc bx2) ax2])
   ;; ax1 bx1 bx2 ax2 => overlap: [bx1 bx2] other: ([ax1 (dec bx1)] [(inc bx2) ax2])
   ;; bx1 ax1 ax2 bx2 => overlap: [ax1 ax2] other: ()
+  ;; (println "subtract-cube" a b)
   (let [result (if (overlap a b)
                  (let [[ox1 ox2 :as overlap-x] [(max ax1 bx1) (min ax2 bx2)]
                        other-x (remove (fn [[min max]] (or (< min ax1) (> max ax2) (> min max)))
@@ -124,63 +125,29 @@
   ;; (println "a-b" (subtract-cube a b))
   ;; (println "b-a" (subtract-cube b a))
   ;; (println "overlap" (list (overlap a b)))
-  (let [result (remove nil? (concat (subtract-cube a b) (subtract-cube b a) (list (overlap a b))))]
+  (let [result (distinct (remove nil? (concat (subtract-cube a b) (subtract-cube b a) (list (overlap a b)))))]
     (if (some (fn [[x1 x2 y1 y2 z1 z2 :as cuboid]] (or (nil? cuboid) (> x1 x2) (> y1 y2) (> z1 z2))) result)
       (println "add-cube" a b "=" result))
     result))
 
 (defn reboot-reactor [instructions]
   (let [cuboids (reduce (fn [cuboids {operation :operation op-cuboid :cuboid :as instruction}]
-                          ;;(println "reduce-fn cuboids" cuboids)
                           (if (= operation "on")
-                            (distinct (apply concat (map #(add-cube op-cuboid %) cuboids)))
+                            ;; subtract each existing cuboid from the new cuboid, then add what's left.
+                            (concat cuboids
+                                    (reduce (fn [op-sub-cuboids cuboid]
+                                              (apply concat (map (fn [op-sub-cuboid] (subtract-cube op-sub-cuboid cuboid)) op-sub-cuboids)))
+                                            (list op-cuboid)
+                                            cuboids))
                             (distinct (apply concat (map #(subtract-cube % op-cuboid) cuboids)))))
                         (list (:cuboid (first instructions)))
                         (rest instructions))]
-    (println "cuboids" (sort-by second (map #(list % (count-cubes %)) cuboids)))
-    (apply + (map count-cubes cuboids))))
+    cuboids))
 
+;; (time (apply + (map count-cubes (reboot-reactor (parse-input small-input-2)))))
+;; "Elapsed time: 170.9503 msecs"
+;; 2758514936282235
 
-;; Right answer for the mini-input:
-;; (time (reboot-reactor mini-input))
-;; "Elapsed time: 1.2437 msecs"
-;; 39
-
-;; Wrong answer for the sample input.
-;; (time (reboot-reactor (parse-input small-input-2)))
-;; "Elapsed time: 1595.4692 msecs"
-;; 50152242282941844
-
-;; [0 1 0 1 0 1] => [0 0 0] [0 0 1] [0 1 0] [0 1 1] [1 0 0] [1 0 1] [1 1 0] [1 1 1]
-;; [1 2 1 2 1 2] => [1 1 1] [1 1 2] [1 2 1] [1 2 2] [2 1 1] [2 1 2] [2 2 1] [2 2 2]
-(defn reboot-debug [instructions]
-  (let [cuboids (reduce (fn [cuboids {operation :operation op-cuboid :cuboid :as instruction}]
-                          (println "reduce-fn cuboids" cuboids op-cuboid)
-                          (if (= operation "on")
-                            ;; non-overlapping cuboids + non-overlapping parts of overlapping cuboids
-                            ;; + non-overlapping parts of op-cuboid + overlaps
-                            (let [{non-overlapping false overlapping true :as grouped} (group-by #(overlaps? % op-cuboid) cuboids)]
-                              (distinct (concat non-overlapping
-                                                (apply concat (map #(subtract-cube % op-cuboid) overlapping))
-                                                (apply concat (map #(subtract-cube op-cuboid %) overlapping))
-                                                (map #(overlap % op-cuboid) overlapping))))
-
-                            ;;(distinct (apply concat (map #(add-cube op-cuboid %) cuboids)))
-
-                            (distinct (apply concat (map #(subtract-cube % op-cuboid) cuboids)))))
-                        (list (:cuboid (first instructions)))
-                        (rest instructions))]
-    cuboids
-    ;; (reduce #(into %1 %2) (map get-cubes cuboids))
-    ))
-
-;; [-22 26 -29 23 -38 16] [-22 -21 -29 23 -38 16]
-
-(defn get-pairs [xs]
-  (if (= (count xs) 2)
-    (list (set xs))
-    (concat (map (fn [x] #{(first xs) x}) (rest xs))
-            (get-pairs (rest xs)))))
-
-(defn any-overlap? [cuboids]
-  (remove nil? (map (fn [s] (let [[a b] (seq s)] (if (overlap a b) (list a b)))) (get-pairs cuboids))))
+;; (time (apply + (map count-cubes (reboot-reactor (parse-input large-input)))))
+;; "Elapsed time: 8122.5505 msecs"
+;; 1130514303649907
