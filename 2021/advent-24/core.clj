062596496bb2aed8a731eb591cade715442bb0d4
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-02-04 09:48:21 -0700

    Solutions for AoC 2021, day 24.

--- 2021/advent-24/core.clj
+++ 2021/advent-24/core.clj
@@ -54,10 +54,10 @@
   ([program inputs] (execute program inputs {"w" 0 "x" 0 "y" 0 "z" 0}))
   ([program inputs init-values]
    (loop [program program inputs inputs variables (into (sorted-map) init-values)]
-     (if (or (empty? program) (empty? inputs))
+     (if (empty? program)
        variables
        (let [{op :op arg1 :arg1 arg2 :arg2} (first program)
-             a (value variables arg1)
+             a (get variables arg1)
              b (value variables arg2)]
          (case op
            "inp" (recur (rest program) (rest inputs) (assoc variables arg1 (first inputs)))
@@ -161,11 +161,23 @@
   (let [v1 (nth v1s idx)
         v2 (nth v2s idx)
         v3 (nth v3s idx)]
-    (filter #(valid-prev-z? (first %))
-            (concat (for [digit (range 1 10)]
-              (list (* v1 (/ (- result digit v3) 26)) digit))
-            (for [digit (range 1 10)]
-              (list (- digit v2) digit))))))
+    (map (fn [[prev-z digit]] (list (long prev-z) digit))
+         (filter #(valid-prev-z? (first %))
+                 (concat
+                  (for [digit (range 1 10)]
+                    (list (* v1 (/ (- result digit v3) 26)) digit))
+                  (for [digit (range 1 10)]
+                    (list (- digit v2) digit)))))))
+
+(defn determine-prev-zs-exhaustive [result idx]
+  (let [v1 (nth v1s idx)
+        v2 (nth v2s idx)
+        v3 (nth v3s idx)]
+    (for [digit (range 1 10)
+          prev-z (range 100000)
+          :let [prev-result (do-math digit prev-z idx)]
+          :when (= prev-result result)]
+      (list prev-z digit))))
 
 ;; For the last digit, digit 13, we're looking for a result of 0, which requires z-div=0:
 ;; z-div is zero when z<v1, non-zero otherwise, so we only need to consider prev-z<26.
@@ -339,16 +351,16 @@
 (defn generate-paths-with-math [full-program path-target]
   (let [{target-num :target path :path :as path-target} path-target
         path-length (count path)]
-    (map (fn [[z d]] {:target z :path (conj path d)}) (determine-prev-zs target-num (- 13 path-length)))))
+    (map (fn [[z d]] {:target z :path (conj path d)}) (determine-prev-zs-exhaustive target-num (- 13 path-length)))))
 
 (defn reverse-monad [full-program digits digit-generator-fn]
   (loop [paths (sorted-set-by compare-path-targets {:target 0 :path []}) iterations 0]
     (let [{target-num :target path :path :as path-target} (first paths)
           path-length (count path)]
-      ;; (println "path-target" path-target)
+      (println "path-target" path-target)
       (cond
         (empty? paths) (format "no paths after %d iterations." iterations)
-        (= digits path-length) path
+        (= digits path-length) (list target-num (reverse path) iterations)
         :else
         (recur (apply conj (disj paths path-target) (digit-generator-fn full-program path-target))
                (inc iterations))))))
@@ -372,3 +384,150 @@
                                       (list (apply min new-zs) (apply max new-zs) (count new-zs)
                                             ;; (apply < new-zs) (some #{0} new-zs)
                                             ))))))))
+
+
+
+;; (def v1s '( 1  1  1  1  26  1 26  1 26 26  26 26  1  26))
+;; (def v2s '(14 11 11 14 -11 12 -1 10 -3 -4 -13 -8 13 -11))
+;; (def v3s '(12  8  7  4   4  1 10  8 12 10  15  4 10   9))
+
+;; pairs: (3 4) (5 6) (7 8) (2 9) (1 10) (0 11) (12 13)
+;; d3 = 7 + d4
+;; d5 = d6
+;; d7 = d8 - 5
+;; d2 = d9 - 3
+;; d1 = d10 + 5
+;; d0 = d11 - 4
+;; d12 = d13 + 1
+
+
+;; d0 5
+;; d1 9
+;; d2 6
+;; d3 9
+;; d4 2
+;; d5 9
+;; d6 9
+;; d7 4
+;; d8 9
+;; d9 9
+;; d10 4
+;; d11 9
+;; d12 8
+;; d13 9
+
+
+(5 9 6 9 2 9 9 4 9 9 4 9 8 9)
+
+;; The zeros for the various pairs.
+;; (([3 4] [8 1] 0)
+;;  ([3 4] [9 2] 0)
+;;  ([5 6] [1 1] 0)
+;;  ([5 6] [2 2] 0)
+;;  ([5 6] [3 3] 0)
+;;  ([5 6] [4 4] 0)
+;;  ([5 6] [5 5] 0)
+;;  ([5 6] [6 6] 0)
+;;  ([5 6] [7 7] 0)
+;;  ([5 6] [8 8] 0)
+;;  ([5 6] [9 9] 0)
+;;  ([7 8] [1 6] 0)
+;;  ([7 8] [2 7] 0)
+;;  ([7 8] [3 8] 0)
+;;  ([7 8] [4 9] 0)
+;;  ([2 9] [1 4] 0)
+;;  ([2 9] [2 5] 0)
+;;  ([2 9] [3 6] 0)
+;;  ([2 9] [4 7] 0)
+;;  ([2 9] [5 8] 0)
+;;  ([2 9] [6 9] 0)
+;;  ([1 10] [6 1] 0)
+;;  ([1 10] [7 2] 0)
+;;  ([1 10] [8 3] 0)
+;;  ([1 10] [9 4] 0)
+;;  ([0 11] [1 5] 0)
+;;  ([0 11] [2 6] 0)
+;;  ([0 11] [3 7] 0)
+;;  ([0 11] [4 8] 0)
+;;  ([0 11] [5 9] 0)
+;;  ([12 13] [2 1] 0)
+;;  ([12 13] [3 2] 0)
+;;  ([12 13] [4 3] 0)
+;;  ([12 13] [5 4] 0)
+;;  ([12 13] [6 5] 0)
+;;  ([12 13] [7 6] 0)
+;;  ([12 13] [8 7] 0)
+;;  ([12 13] [9 8] 0))
+
+;; The results of sub-program 0 for the nine digits.
+;; (([1] 13) ([2] 14) ([3] 15) ([4] 16) ([5] 17) ([6] 18) ([7] 19) ([8] 20) ([9] 21))
+
+;; Results where 3+4 do not modify 0.
+;; (([0 3 4] [1 8 1] 13)
+;;  ([0 3 4] [1 9 2] 13)
+;;  ([0 3 4] [2 8 1] 14)
+;;  ([0 3 4] [2 9 2] 14)
+;;  ([0 3 4] [3 8 1] 15)
+;;  ([0 3 4] [3 9 2] 15)
+;;  ([0 3 4] [4 8 1] 16)
+;;  ([0 3 4] [4 9 2] 16)
+;;  ([0 3 4] [5 8 1] 17)
+;;  ([0 3 4] [5 9 2] 17)
+;;  ([0 3 4] [6 8 1] 18)
+;;  ([0 3 4] [6 9 2] 18)
+;;  ([0 3 4] [7 8 1] 19)
+;;  ([0 3 4] [7 9 2] 19)
+;;  ([0 3 4] [8 8 1] 20)
+;;  ([0 3 4] [8 9 2] 20)
+;;  ([0 3 4] [9 8 1] 21)
+;;  ([0 3 4] [9 9 2] 21))
+
+
+(def program-pairs '([3 4] [5 6] [7 8] [2 9] [1 10] [0 11] [12 13]))
+
+(defn generate-zero-pairs [full-program]
+  (let [sub-programs (partition 18 full-program)]
+    (apply merge-with #(if (seq? %1) (concat %1 (list %2)) (list %1 %2))
+           (for [[idx-1 idx-2] program-pairs
+                 i (range 1 10)
+                 j (range 1 10)
+                 :let [mini-program (concat (nth sub-programs idx-1) (nth sub-programs idx-2))
+                       result (execute-z mini-program (list i j))]
+                 :when (= result 0)]
+             {[idx-1 idx-2] [i j]}))))
+
+(defn generate-paths-from-zero-pairs [pair-keys zero-pairs]
+  (if (= 1 (count pair-keys))
+    (map #(list (list (first pair-keys) %)) (get zero-pairs (first pair-keys)))
+    (apply concat
+     (for [pair (get zero-pairs (first pair-keys))]
+       (map #(apply concat (list (list (list (first pair-keys) pair)) %)) (generate-paths-from-zero-pairs (rest pair-keys) zero-pairs))))))
+
+(defn pairs-to-digits [places-and-digits]
+  (reduce (fn [acc [[p1 p2] [d1 d2]]] (-> acc
+                                          (assoc p1 d1)
+                                          (assoc p2 d2)))
+          [0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+          places-and-digits))
+
+(defn generate-full-paths [full-program]
+  (let [zero-pairs (generate-zero-pairs full-program)]
+    (generate-paths-from-zero-pairs (keys zero-pairs) zero-pairs)))
+
+;; From the reddit thread, I learned to identify pairs of sub-programs that cancel each other out. From there,
+;; I just had to figure out which digits worked for each cancelling pair of sub-programs, and then come up with
+;; all the possible combinations of those digits. The first one (last one) was the answer.
+
+;; (first (reverse (sort (map pairs-to-digits (generate-full-paths (parse-input large-input))))))
+;; [5 9 6 9 2 9 9 4 9 9 4 9 9 8]
+;; (execute-z (parse-input large-input) (first (reverse (sort (map pairs-to-digits (generate-full-paths (parse-input large-input)))))))
+;; 0
+;; 59692994994998
+
+
+;; (first (sort (map pairs-to-digits (generate-full-paths (parse-input large-input)))))
+;; [1 6 1 8 1 1 1 1 6 4 1 5 2 1]
+;; (execute-z (parse-input large-input) (first (sort (map pairs-to-digits (generate-full-paths (parse-input large-input))))))
+;; 0
+;; 16181111641521
+
