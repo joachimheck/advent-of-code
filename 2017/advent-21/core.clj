b34397372169e0c1e016ca675bb2d47685c13381
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-02-16 07:34:00 -0700

    Solutions for AoC 2017, day 21.

--- 2017/advent-21/core.clj
+++ 2017/advent-21/core.clj
@@ -29,17 +29,38 @@
        (map vec)
        (into {})))
 
+(def test-pattern [(vec "abcd")
+                   (vec "efgh")
+                   (vec "ijkl")
+                   (vec "mnop")])
+
 (defn split-pattern [pattern]
   (let [pattern-size (count (first pattern))
         new-size (if (= 0 (mod pattern-size 2)) 2 3)
         rows-split (mapv #(partition new-size %) pattern)
         steps (range (quot pattern-size new-size))]
-    (apply concat
-     (for [j steps]
-       (for [i steps]
-         (vec
-          (for [k (range new-size)]
-            (vec (nth (nth rows-split (+ (* j new-size) k)) i)))))))))
+    (vec
+     (apply concat
+            (for [j steps]
+              (for [i steps]
+                (vec
+                 (for [k (range new-size)]
+                   (vec (nth (nth rows-split (+ (* j new-size) k)) i))))))))))
+
+(defn merge-patterns [patterns]
+  (if (= 1 (count patterns))
+    (first patterns)
+    (let [rows (int (Math/sqrt (count patterns)))
+          columns rows
+          sub-rows (count (first patterns))]
+      (vec
+       (apply concat
+              (for [j (range rows)]
+                (for [k (range sub-rows)]
+                  (vec
+                   (apply concat
+                    (for [i (range columns)]
+                      (get-in patterns [(+ (* j columns) i) k])))))))))))
 
 (defn flip-pattern [pattern]
   (map vec (list pattern
@@ -71,7 +92,44 @@
 (defn rotate-and-flip [pattern]
   (distinct (apply concat (map rotate-pattern (flip-pattern pattern)))))
 
-;; TODO: stitch sub-patterns back together into a larger pattern.
+(defn to-rule [pattern]
+  (str/join "/" (map str/join pattern)))
+
+(defn to-pattern [rule]
+  (mapv vec (str/split rule #"/")))
+
+(defn match [pattern rules]
+  (let [patterns (rotate-and-flip pattern)
+        match (first (filter #(contains? rules (to-rule %)) patterns))]
+    (to-pattern (get rules (to-rule match)))))
 
 (defn enhance [pattern rules]
-  )
+  (let [patterns (split-pattern pattern)
+        matches (mapv #(match % rules) patterns)
+        merged (merge-patterns matches)]
+    merged))
+
+(defn count-pixels [pattern]
+  (list (get (frequencies (flatten pattern)) \#)
+        (format "%dx%d" (count pattern) (count pattern))))
+
+(defn multi-enhance [pattern rules iterations]
+  (nth (iterate #(enhance % rules) pattern) iterations))
+
+(defn draw-pattern [pattern]
+  (str/join "\n" (map str/join pattern)))
+
+;; (time (count-pixels (multi-enhance initial-pattern (parse-input large-input) 5)))
+;; "Elapsed time: 11.4253 msecs"
+;; 147
+
+
+
+;; Part 2
+;; How many pixels are on after 18 iterations?
+
+
+
+;; (time (count-pixels (multi-enhance initial-pattern (parse-input large-input) 18)))
+;; "Elapsed time: 71718.8116 msecs"
+;; (1936582 "2187x2187")
