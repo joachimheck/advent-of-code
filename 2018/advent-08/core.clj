be2a54ce3fbfab75eaf7770d0ff3a0152f861b36
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-02-23 06:33:21 -0700

    Solutions for AoC 2018, day 8.

--- 2018/advent-08/core.clj
+++ 2018/advent-08/core.clj
@@ -24,8 +24,38 @@
 (defn build-tree [t-vec]
   (let [child-count (first t-vec)
         meta-count (second t-vec)]
-    (loop [rest-vec (drop 2 t-vec) i 0 children '()]
+    (loop [rest-vec (drop 2 t-vec) i 0 children []]
       (if (= i child-count)
-        (list child-count meta-count children rest-vec)
-        (let [subtree (build-tree rest-vec)]
-          (recur (last subtree) (inc i) (conj children subtree)))))))
+        (list (list children (take meta-count rest-vec)) (drop meta-count rest-vec))
+        (let [[node new-rest-vec] (build-tree rest-vec)]
+          (recur new-rest-vec (inc i) (conj children node)))))))
+
+(defn count-metadata [t-vec]
+  (apply + (flatten (build-tree t-vec))))
+
+;; (time (count-metadata (parse-input small-input)))
+;; "Elapsed time: 0.9546 msecs"
+;; 138
+
+;; (time (count-metadata (parse-input large-input)))
+;; "Elapsed time: 71.1256 msecs"
+;; 46578
+
+
+
+;; Part 2
+;; Find the value of the root node.
+(defn value [[children metadata :as node]]
+  (if (empty? children)
+    (apply + metadata)
+    (let [child-count (count children)
+          indices (filter #(< -1 % child-count) (map dec metadata))]
+      (apply + (map #(value (nth children %)) indices)))))
+
+;; (time (value (first (build-tree (parse-input small-input)))))
+;; "Elapsed time: 0.8314 msecs"
+;; 66
+
+;; (time (value (first (build-tree (parse-input large-input)))))
+;; "Elapsed time: 17.2722 msecs"
+;; 31251
