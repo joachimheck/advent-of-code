5bf826c2726a92aa5fb2578e8c12380ad3bd1729
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-03-11 10:48:20 -0700

    Solutions to AoC 2018, day 21.

--- 2018/advent-21/core.clj
+++ 2018/advent-21/core.clj
@@ -107,22 +107,30 @@
   [a b c registers]
   (assoc registers c (if (= (get registers a) (get registers b)) 1 0)))
 
+;; (defn unop
+;;   "(unknown operation) sets register C to the result of (compute-r3 A B)."
+;;   [a b c registers]
+;;   (assoc registers c (compute-r3 (get registers a) (get registers b))))
+
 (defn get-fn-name [fn]
   (second (re-matches #".+\$(.+?)\@.+" (str fn))))
 
-(defn run-program [{:keys [ip-register instructions] :as input} initial-registers]
+(defn run-program [{:keys [ip-register instructions] :as input} initial-registers max-iterations]
   (loop [ip 0
          registers initial-registers
          instructions instructions
          i 0]
-    (if (< -1 ip (count instructions))
-      (let [[op a b c :as instruction] (nth instructions ip)
-            ip-updated (assoc registers ip-register ip)
-            new-registers (op a b c ip-updated)
-            new-ip (get new-registers ip-register)]
-        (printf "ip=%d %s %s %d %d %d %s\n" ip ip-updated (get-fn-name op) a b c new-registers)
-        (recur (inc new-ip) new-registers instructions (inc i)))
-      registers)))
+    (if (= i max-iterations)
+      (get registers 3)
+      (if (< -1 ip (count instructions))
+        (let [[op a b c :as instruction] (nth instructions ip)
+              ip-updated (assoc registers ip-register ip)
+              new-registers (op a b c ip-updated)
+              new-ip (get new-registers ip-register)]
+          ;; (if (= ip 28)
+          ;;   (printf "ip=%d %s %s %d %d %d %s\n" ip ip-updated (get-fn-name op) a b c new-registers))
+          (recur (inc new-ip) new-registers instructions (if (= ip 28) (inc i) i)))
+        registers))))
 
 (defn run-decompiled-program [r0]
   (let [registers [r0 0 0 0 0 0 false]]
@@ -150,14 +158,74 @@
           registers
           (recur registers))))))
 
-(defn compute-r3 [r3 r4]
-  (bit-and (* (bit-and (+ r3 r4) 16777215) 65899) 16777215))
+(defn compute-r3 [r3 r2]
+  (bit-and (* (bit-and (+ r3 (bit-and r2 255)) 16777215) 65899) 16777215))
 
-(defn compute-r234 [r2 r3 r4]
-  (loop [r2 r2 r3 r3 r4 r4]
-    (if (< r2 256)
-      [(+ r4 1) r3 r4]
-      (let [new-r4 (bit-and r2 255)
-            new-r3 (compute-r3 r3 new-r4)
-            new-r2 (inc (quot r2 256))]
-        (recur new-r2 new-r3 new-r4)))))
+(defn compute-r23 [r2 r3]
+  (loop [r2 r2 r3 r3 i 0]
+    ;; (println "compute-r23" r2 r3 )
+    (if (= i 10)
+      :max-iterations
+      (let [new-r3 (compute-r3 r3 r2)]
+        (if (< r2 256)
+          [r2 new-r3]
+          (let [new-r2 (inc (quot r2 256))]
+            (recur new-r2 new-r3 (inc i))))))))
+
+(defn program [r0]
+  (loop [r3 0 i 0]
+    (if (= i 10)
+      :max-iterations
+      (let [r2 (bit-or r3 65536)
+            r3 1505483
+            [new-r2 new-r3] (compute-r23 r2 r3)]
+        (println "comparing" new-r3 "to expected" r0)
+        (if (= new-r3 r0)
+          new-r3
+          (recur new-r3 (inc i)))))))
+
+;; 4662112 ; this is the first r3 my program spits out.
+;; ---> answer <---
+
+;; My simulated program doesn't seem to be giving the right answers. I ran the real program,
+;; printing out the registers at the end of the big loop, and the first number it printed, 7216956, was the right one.
+
+;; (run-program (parse-input large-input) [0 0 0 0 0 0] 10000)
+;; ip=28 [0 28 1 7216956 1 1] eqrr 3 0 4 [0 28 1 7216956 0 1]
+;; :max-iterations
+
+
+
+;; Part 2
+;; What is the lowest non-negative integer value for register 0 that causes the program to halt after executing the most instructions?
+
+(defn collect-r3s [{:keys [ip-register instructions] :as input}]
+  (loop [ip 0
+         registers [0 0 0 0 0 0]
+         instructions instructions
+         i 0
+         r3-values []]
+    (if (< -1 ip (count instructions))
+      (let [[op a b c :as instruction] (nth instructions ip)
+            ip-updated (assoc registers ip-register ip)
+            new-registers (op a b c ip-updated)
+            new-ip (get new-registers ip-register)]
+        (if (= ip 28)
+          (let [new-i (inc i)
+                new-r3 (get registers 3)]
+            (if (= 0 (mod i 16)) (println "i =" i))
+            (if (some #{new-r3} r3-values)
+              r3-values
+              (recur (inc new-ip) new-registers instructions new-i (conj r3-values new-r3))))
+          (recur (inc new-ip) new-registers instructions i r3-values)))
+      {:state "exited" :registers registers})))
+
+;; ---> answer <---
+;; 16747615
+
+
+;; advent-21.core> (time (collect-r3s (parse-input large-input)))
+;; "Elapsed time: 7920207.554 msecs"
+;; [7216956
+;;  ... (10776 entries) ...
+;;  14596916]
