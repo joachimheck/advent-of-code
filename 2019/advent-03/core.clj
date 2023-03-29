5c85310b093b62bbb4d63538248f60b4d01b69b3
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-03-29 07:45:13 -0600

    Work on AoC 2019, day 3.

---
+++ 2019/advent-03/core.clj
@@ -0,0 +1,20 @@
+(ns advent-03.core)
+
+(require '[clojure.pprint :as pp])
+(require '[clojure.repl :refer :all])
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+(require '[clojure.test :refer :all])
+(require '[clojure.instant :as instant])
+
+(def small-input "small-input.txt")
+(def large-input "large-input.txt")
+
+(defn read-lines [f]
+  (with-open [rdr (clojure.java.io/reader f)]
+    (doall(line-seq rdr))))
+
+;; Day 3: Crossed Wires
+
+;; Part 1
+;; 
