96fa6cbe9b862576b2f9f09da15f3e33d1ae8001
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-04-06 07:21:58 -0600

    Solution to AoC 2019, day 10, part 2. I had an idea but it didn't work; after looking up the answer on the internet, I realized my code was rotating in the wrong diretion.

--- 2019/advent-10/core.clj
+++ 2019/advent-10/core.clj
@@ -44,13 +44,6 @@
 (defn gcd [a b]
   (apply max (concat '(1) (set/intersection (set (factors a)) (set (factors b))))))
 
-;; (defn is-occluded-by? [[a b] [c-orig d-orig]]
-;;   (let [[c d] [(quot c-orig (gcd c-orig d-orig)) (quot d-orig (gcd c-orig d-orig))]]
-;;    (and (not= [a b] [c-orig d-orig])
-;;         (or (= 0 a c) (and (not= 0 c) (= 0 (mod a c)) (> (quot a c-orig) 0)))
-;;         (or (= 0 b d) (and (not= 0 d) (= 0 (mod b d)) (> (quot b d-orig) 0)))
-;;         (or (= 0 c) (= 0 d) (= (quot a c) (quot b d))))))
-
 (defn is-occluded-by? [[a b] [c d]]
   (and (<= (distance [0 0] [c d]) (distance [0 0] [a b]))
    (cond (= 0 a c) (or (< 0 d b) (> 0 d b))
@@ -66,7 +59,6 @@
 (defn remove-occluded [diffs]
   (reduce (fn [diffs current]
             (remove (fn [diff]
-                      ;; (println "Is" diff "occluded by" current "?" (is-occluded-by? diff current))
                       (is-occluded-by? diff current)) diffs))
           diffs
           diffs))
@@ -138,8 +130,13 @@
 (defn round-it [n]
   (/ (Math/round (* n 65536)) 65536))
 
+(defn normalize [theta]
+  (if (< theta 0)
+    (+ theta (* 2 Math/PI))
+    theta))
+
 (defn to-polar [x y]
-  [(Math/sqrt (+ (* x x) (* y y))) (round-it (Math/atan2 y x))])
+  [(Math/sqrt (+ (* x x) (* y y))) (normalize (Math/atan2 x (- y)))])
 
 (def small-input-6 "small-input-6.txt")
 
@@ -149,30 +146,89 @@
          i 0]
     (if (or (= 20 i) (empty? asteroids))
       vaporized
-      (let [data (sort-by last
-                          (map (fn [[x y]]
-                                 (let [[r theta] (to-polar (- x ax) (- y ay))]
-                                   (list [x y]
-                                         [r theta]
-                                         (if (< (+ theta (round-it (/ Math/PI 2))) 0) (round-it (+ theta (* 2 Math/PI))) theta))))
-                               (visible-from asteroid asteroids)))
-            new-asteroids (remove (set (map first data)) asteroids)
-            vaporized (vec (concat vaporized (map first data)))]
-        (println "Vaporized" (count data) "asteroids," (count new-asteroids) "remaining.")
+      (let [visible (set (visible-from asteroid asteroids))
+            sorted-visible (sort-by last
+                                    (map (fn [[x y]]
+                                           (let [[r theta] (to-polar (- x ax) (- y ay))]
+                                             (list [x y]
+                                                   [r theta]
+                                                   theta
+                                                   ;; (if (< (+ theta (round-it (/ Math/PI 2))) 0) (round-it (+ theta (* 2 Math/PI))) theta)
+                                                   )))
+                                         visible))
+            new-asteroids (remove visible asteroids)
+            vaporized (vec (concat vaporized (map first sorted-visible)))]
+        (println "Vaporized" (count sorted-visible) "asteroids," (count new-asteroids) "remaining.")
         (recur new-asteroids vaporized (inc i))))))
 
 ;; ---> answer <---
 ;; 2600
 ;; Also wrong:
-;; ([0 7] 7) ([4 10] 410)
+;; ([0 7] 7) ([4 10] 410) ([22 34] 2234) ([21 35] 2135)
 
 (def test-input "test-input.txt")
 (def test-input-2 "test-input-2.txt")
 
 (defn find-200th-vaporized [asteroid asteroids]
-  (let [[x y] (nth (lazerize-asteroids asteroid asteroids) 200)]
+  (let [[x y] (nth (lazerize-asteroids asteroid asteroids) (dec 200))]
     (list [x y] (+ (* 100 x) y))))
 
 (defn find-last-vaporized [asteroid asteroids]
   (let [[x y] (last (lazerize-asteroids asteroid asteroids))]
     (list [x y] (+ (* 100 x) y))))
+
+(deftest test-find-last-vaporized
+  (is (= '([14 3] 1403) (find-last-vaporized [8 3] (parse-input small-input-6))))
+  (is (= '([11 1] 1101) (find-last-vaporized [11 13] (parse-input small-input-5)))))
+
+
+
+
+;; I don't know how to debug this. I'll try getting the right answer from the internet, at least.
+;; Looks like it's [14 19]. My code gets that as the 100th asteroid, not the 200th.
+(defn polar
+  [[sx sy :as source] [tx ty :as target]]
+  {:r (apply + (map (comp #(Math/abs %) -) source target)) ;; Close enough for our purposes
+   :theta (- Math/PI (apply #(Math/atan2 %1 %2) ^:double (map (comp double -) target source)))
+   :asteroid target})
+
+(defn polar-lines
+  [input station]
+  (let [asteroids input
+        polars (for [target asteroids
+                     :when (not= station target)]
+                 (polar station target))]
+    (->> polars
+         (group-by :theta)
+         (sort-by first)
+         (map (comp (partial map :asteroid) (partial sort-by :r) second)))))
+
+(defn laser-until [lines n]
+  (loop [[[next-asteroid & rest-asteroids] & rest-lines] lines
+         vaporized []
+         n n
+         i 1]
+   (if (= n 0)
+     vaporized
+     (let [line' (if (empty? rest-asteroids) [] [rest-asteroids])
+           lines' (concat rest-lines line')]
+       (recur lines' (conj vaporized (list i (list next-asteroid (+ (* 100 (first next-asteroid)) (second next-asteroid))))) (dec n) (inc i))))))
+
+
+(let [input input]
+  (-> input
+      (polar-lines station)
+      (laser-until 200)))
+
+
+;; I was turning counter-clockwise.
+;; (find-200th-vaporized [26 29] (parse-input large-input))
+;; Vaporized 299 asteroids, 48 remaining.
+;; Vaporized 30 asteroids, 18 remaining.
+;; Vaporized 9 asteroids, 9 remaining.
+;; Vaporized 3 asteroids, 6 remaining.
+;; Vaporized 2 asteroids, 4 remaining.
+;; Vaporized 2 asteroids, 2 remaining.
+;; Vaporized 1 asteroids, 1 remaining.
+;; Vaporized 1 asteroids, 0 remaining.
+;; ([14 19] 1419)
