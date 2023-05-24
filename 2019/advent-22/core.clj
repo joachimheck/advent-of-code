14a525a9fa856b11cb0771fdfac2701c50e342d2
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-05-24 07:28:10 -0600

    Work on AoC 2019, day 22, part 2.

--- 2019/advent-22/core.clj
+++ 2019/advent-22/core.clj
@@ -108,14 +108,64 @@
          :when (= (mod (* i increment) m) n)]
      i)))
 
+;; All the digits in a cycle have the same modulus: (mod d increment) = C.
+
+;; TODO
+;; What digits are in that cycle, and where is the digit at p in the cycle?
+
+;; digits: 0 3 6 9 2 5 8 1 4 7
+;; posns:  0 1 2 3 4 5 6 7 8 9
+;; mod7:   0 1 2 3 4 5 6 0 1 2
+
+;; rem:    3
+;; mod for cycle 0 = 0
+;; mod for cycle 1 = n-rem = 7-3 = 4
+;; mod for cycle 2 = n-2*rem = 7-6 = 1
+;; mod for cycle 3 = n-3*rem mod n = 7-9 mod 7 = -2 mod 7 = 5
+;; mod for cycle 4 = n-4*rem mod n = 2
+;; mod for cycle 5 = 6
+;; mod for cycle 6 = 3
+
+;; mod7:   0 0 4 1 1 5 2 2 6 3
+
+(defn mod-for-cycle [cycle card-count increment]
+  (mod (- increment (* cycle (rem card-count increment))) increment))
+
+(defn cards-in-cycle [cycle card-count increment]
+  (+ 1 (quot (dec (- card-count (mod-for-cycle cycle card-count increment))) increment)))
+
+;; What's the inverse of mod-for-cycle?
+(defn cycle-for-mod [m card-count increment]
+  ;; mod-for-cycle: 0 4 1 5 2 6 3 0 4 1
+  ;; cycle-for-mod: 0 0 4 1 1 5 2 2 6 3
+
+  ;; (mod (- increment (* cycle (rem card-count increment))) increment) = m
+  ;; (- increment (* cycle (rem card-count increment))) = (+ (* n increment) m) ; for any n
+  ;; (- increment (* cycle (rem card-count increment))) = (+ (* 0 increment) m)
+  ;; (- increment (* cycle (rem card-count increment))) = m
+  ;; (- (* cycle (rem card-count increment))) = (- m increment)
+  ;; (* cycle (rem card-count increment)) = (- increment m)
+  ;; cycle = (/ (- increment m) (rem card-count increment))
+  (mod (- increment (* m (rem card-count increment))) increment)
+)
+
+
+
 (defn before-deal-with-increment [position card-count n]
   (let [
+        (mod position n)
+
         tuple (quot position n)
         position-in-tuple (mod position n)
         advancement (- n (rem card-count n))
-        cycle (compute-cycle position-in-tuple n advancement)]
+        ;; cycle (compute-cycle position-in-tuple n advancement)
+        cycle
+        ]
+
     ;; (list tuple position-in-tuple advancement cycle)
-    (+ (+ (* cycle n) tuple) (if (= 0 (mod position n)) 0 1))))
+    (println "position" position "tuple" tuple "position-in-tuple" position-in-tuple "cycle" cycle "mod" (mod position n)
+             "p mod n" (mod position n) "rem" (rem card-count n))
+    (+ (* cycle (quot card-count n)) tuple)))
 
 ;; I have the (hopefully correct) reverse functions. I still need to chain them together
 ;; in the reverse order of the given instructions, and figure out how to loop them.
