3d677c73f937ca92adb16d335271fb88e42cfbb8
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-06-03 08:53:54 -0600

    Work on AoC 2019, day 25.
    The program runs but crashes sometimes; I'm not sure whether that's correct.

---
+++ 2019/advent-25/core.clj
@@ -0,0 +1,171 @@
+(ns advent-25.core)
+
+(require '[clojure.pprint :as pp])
+(require '[clojure.repl :refer :all])
+(require '[clojure.string :as str])
+(require '[clojure.set :as set])
+(require '[clojure.test :refer :all])
+(require '[clojure.instant :as instant])
+(require '[clojure.walk :as walk])
+
+(def small-input "small-input.txt")
+(def large-input "large-input.txt")
+
+(defn read-lines [f]
+  (with-open [rdr (clojure.java.io/reader f)]
+    (doall(line-seq rdr))))
+
+;; Day 23: Cryostasis
+
+;; Part 1
+;; 
+(defn parse-line [line]
+  (reduce (fn [m [k v]] (assoc m k v)) {} (map-indexed list (load-string (str/join (list "[" line "]"))))))
+
+(defn parse-input [f]
+  (parse-line (first (read-lines f))))
+
+(defn pad-array [array]
+  (let [remainder (rem (count array) 4)]
+    (if (= remainder 0)
+      array
+      (apply conj array (vec (repeat (- 4 remainder) 0))))))
+
+(defn digits [n]
+  (if (< n 10)
+    [n]
+    (let [quotient (quot n 10)
+          remainder (rem n 10)]
+      (conj (digits quotient) remainder))))
+
+(defn parse-operation [n]
+  (let [n-digits (digits n)
+        opcode (if (= (count n-digits) 1)
+                 n
+                 (+ (* 10 (nth n-digits (- (count n-digits) 2))) (last n-digits)))
+        p-modes (vec (reverse (drop-last 2 n-digits)))]
+    {:opcode opcode
+     :p-modes p-modes}))
+
+(def POSITION 0)
+(def IMMEDIATE 1)
+(def RELATIVE 2)
+
+(defn get-value [n memory p-mode relative-base]
+  (cond (= p-mode POSITION) (let [address n]
+                              (if (< address 0)
+                                "Negative address"
+                                (get memory address 0)))
+        (= p-mode IMMEDIATE) n
+        (= p-mode RELATIVE) (let [address (+ relative-base n)]
+                              (if (< address 0)
+                                "Negative address"
+                                (get memory address 0)))
+        :else "Unknown p-mode"))
+
+(defn get-address [n p-mode relative-base]
+  (cond (= p-mode POSITION) n
+        (= p-mode RELATIVE) (+ relative-base n)
+        :else "Unknown p-mode"))
+
+(defn op-add [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 4)
+         :memory (assoc memory (get-address c (get p-modes 2 0) relative-base)
+                        (+ (get-value a memory (get p-modes 0 0) relative-base)
+                           (get-value b memory (get p-modes 1 0) relative-base)))))
+
+(defn op-mul [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 4)
+         :memory (assoc memory
+                        (get-address c (get p-modes 2 0) relative-base)
+                        (* (get-value a memory (get p-modes 0 0) relative-base)
+                           (get-value b memory (get p-modes 1 0) relative-base)))))
+
+(defn op-in [a b c p-modes {:keys [ip memory inputs relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 2)
+         :memory (assoc memory (get-address a (get p-modes 0 0) relative-base) (* 1 (first inputs)))
+         :inputs (vec (rest inputs))))
+
+(defn op-out [a b c p-modes {:keys [ip memory outputs relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 2)
+         :outputs (conj outputs (get-value a memory (get p-modes 0 0) relative-base))))
+
+(defn op-jt [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (if (not= 0 (get-value a memory (get p-modes 0 0) relative-base))
+               (get-value b memory (get p-modes 1 0) relative-base)
+               (+ ip 3))))
+
+(defn op-jf [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (if (= 0 (get-value a memory (get p-modes 0 0) relative-base))
+               (get-value b memory (get p-modes 1 0) relative-base)
+               (+ ip 3))))
+
+(defn op-lt [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 4)
+         :memory (assoc memory (get-address c (get p-modes 2 0) relative-base)
+                        (if (< (get-value a memory (get p-modes 0 0) relative-base)
+                               (get-value b memory (get p-modes 1 0) relative-base))
+                          1
+                          0))))
+
+(defn op-eq [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 4)
+         :memory (assoc memory (get-address c (get p-modes 2 0) relative-base)
+                        (if (= (get-value a memory (get p-modes 0 0) relative-base)
+                               (get-value b memory (get p-modes 1 0) relative-base))
+                          1
+                          0))))
+
+(defn op-rb [a b c p-modes {:keys [ip memory relative-base] :as state}]
+  (assoc state
+         :ip (+ ip 2)
+         :relative-base (+ relative-base (get-value a memory (get p-modes 0 0) relative-base))))
+
+(defn op-ex [a b c p-modes state]
+  (assoc state :ip -1))
+
+(def ops-by-code {1 op-add
+                  2 op-mul
+                  3 op-in
+                  4 op-out
+                  5 op-jt
+                  6 op-jf
+                  7 op-lt
+                  8 op-eq
+                  9 op-rb
+                  99 op-ex
+                  })
+
+(defn do-op [opcode a b c p-modes state]
+  (if (nil? (get ops-by-code opcode)) (println "no op for opcode" opcode))
+  ((get ops-by-code opcode) a b c p-modes state))
+
+(defn run-program [program]
+  (loop [state {:ip 0 :memory program :inputs [] :outputs [] :relative-base 0}]
+    ;; (println "memory" (sort (:memory state)))
+    ;; (println "loop ip" (:ip state) "rb" (:relative-base state) "outputs" (:outputs state))
+    ;; (println "loop" "memory" memory "inputs" inputs "outputs" outputs)
+    (let [{:keys [ip memory]} state
+          [operation a b c] (for [i (range 4)] (get memory (+ ip i) 0))
+          {:keys [opcode p-modes]} (parse-operation operation)
+          ;; {:keys [ip outputs] :as new-state} (do-op opcode a b c p-modes state)
+          ]
+      (if (and (= op-in (get ops-by-code opcode)) (empty? (:inputs state)))
+        (do (print (str/join (map char (:outputs state))))
+            (flush)
+            (let [user-input (read-line)
+                  _ (println "> " user-input)
+                  state-with-input (-> state
+                                       (assoc :inputs (conj (mapv int user-input) 10))
+                                       (assoc :outputs []))]
+              (recur (do-op opcode a b c p-modes state-with-input))))
+        (recur (do-op opcode a b c p-modes state))))))
+
