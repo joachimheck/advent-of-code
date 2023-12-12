9ac7216678378017cefe849d3aed9249b0c4c637
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-12-12 11:20:34 -0700

    Solution for AoC 2023, day 12, part 1.

--- 2023/advent-12/core.clj
+++ 2023/advent-12/core.clj
@@ -20,13 +20,51 @@
        (map #(str/split % #" "))
        (map (fn [[a b]] [a (load-string (str/join ["[" b "]"]))]))))
 
-(defn trim-line [springs sizes]
-  (loop [springs springs
-         sizes sizes]
-    (let [end (re-matches #".*?(\.?\#+\.?)$" springs)]
-      (if (seq end)
-        (recur (subs springs 0 (- (count springs) (count (second end)))) (butlast sizes))
-        (let [start (re-matches #"^(\.?\#+\.?).*" springs)]
-          (if (seq start)
-            (recur (subs springs (count (second start))) (rest sizes))
-            [springs sizes]))))))
+(defn springs-to-allocate [springs sizes]
+  (let [known (count (filter #{\#} springs))
+        unknown (count (filter #{\?} springs))]
+    (- (apply + sizes) known)))
+
+(defn spring-permutations [spring-count positions]
+  (if (= spring-count 0)
+    '(())
+    (let [starts (take (inc (- (count positions) spring-count)) positions)]
+      (apply concat
+       (for [start starts]
+         (map #(conj % start)
+              (spring-permutations (dec spring-count) (filter #(> % start) positions))))))))
+
+(defn is-valid? [springs sizes]
+  (= sizes (map count (str/split (str/replace springs #"^\.+|\.+$" "") #"\.+"))))
+
+(defn populate-springs [springs position-sets pos-map]
+  (for [ps position-sets]
+    (str/replace (reduce (fn [acc p] (str (subs acc 0 (get pos-map p)) "#" (subs acc (inc (get pos-map p)))))
+                         springs
+                         ps)
+                 #"\?" ".")))
+
+(defn arrangements [springs sizes]
+  (let [to-allocate (springs-to-allocate springs sizes)
+        pos-map (into {} (map (fn [[n [p _]]] [n p]) (map-indexed list (filter #(= \? (second %)) (map-indexed list springs)))))
+        positions (count pos-map)
+        position-sets (spring-permutations to-allocate (range 0 positions))
+        guesses (populate-springs springs position-sets pos-map)
+        arrangements (filter #(is-valid? % sizes) guesses)]
+    (count arrangements)))
+
+(defn sum-arrangement-counts [input]
+  (let [state (parse-input input)]
+    (apply + (map #(apply arrangements %) state))))
+
+;; (time (sum-arrangement-counts small-input))
+;; "Elapsed time: 10.177 msecs"
+;; 21
+
+;; (time (sum-arrangement-counts large-input))
+;; "Elapsed time: 8413.2694 msecs"
+;; 7771
+
+
+
+;; Part 2
