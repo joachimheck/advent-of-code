74e909ca8ef4439948d5c890762165dff6a25456
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-12-27 07:29:11 -0700

    Work on AoC 2023, day 12, part 2.

--- 2023/advent-12/core.clj
+++ 2023/advent-12/core.clj
@@ -77,18 +77,18 @@
        (map (fn [[a b]] [(str/join "?" (repeat 5 a))
                          (load-string (str/join ["[" (str/join "," (repeat 5 b)) "]"]))]))))
 
-(defn arrange-recursively [springs sizes]
-  (str/split springs #"\.")
-  (loop [open-set [[springs sizes ""]]
-         results []]
-    (let [[springs-left sizes-left reconstructed :as current] (first open-set)]
-      (cond (empty? springs-left) (recur (rest open-set) (conj results reconstructed))
-            (str/starts-with? springs-left ".")
-            (let [[_ dots more] (re-matches #"^(\.+)(.*)" springs-left)]
-               (recur (assoc open-set 0 [more sizes-left (str reconstructed dots)])))
-            :else
-            :todo
-))))
+;; (defn arrange-recursively [springs sizes]
+;;   (str/split springs #"\.")
+;;   (loop [open-set [[springs sizes ""]]
+;;          results []]
+;;     (let [[springs-left sizes-left reconstructed :as current] (first open-set)]
+;;       (cond (empty? springs-left) (recur (rest open-set) (conj results reconstructed))
+;;             (str/starts-with? springs-left ".")
+;;             (let [[_ dots more] (re-matches #"^(\.+)(.*)" springs-left)]
+;;                (recur (assoc open-set 0 [more sizes-left (str reconstructed dots)])))
+;;             :else
+;;             :todo
+;; ))))
 
 ;; This idea might work in principle but I guess it will have to realize each of the
 ;; possible arrangements, so it might run out of memory (or just still be slow).
@@ -97,17 +97,38 @@
 ;; Here's my implementation of a solution from:
 ;; https://www.reddit.com/r/adventofcode/comments/18h4ign/2023_day_12_part_2_how_to_approach_part_2/
 (defn possible-arrangements [springs sizes run-length]
-  (println "possible-arrangements" springs sizes run-length)
-  (if (empty? springs) (println "empty springs" sizes run-length))
-  (println "first:" (first springs))
-  (case (first springs)
-    nil (if (= (inc run-length) (first sizes)) 1 0)
-    \. (+ (if (= (inc run-length) (first sizes)) 1 0)
-          (possible-arrangements (subs springs 1) sizes 0))
-    \# (if (= (inc run-length) (first sizes))
-         (inc (possible-arrangements (subs springs 1) (rest sizes) 0))
-         (possible-arrangements (subs springs 1) sizes (inc run-length)))
-    \? (+ (possible-arrangements (subs springs 1) sizes 0)
-          (possible-arrangements (subs springs 1) sizes (inc run-length)))))
+  ;; (println "possible-arrangements" springs sizes run-length)
+  (if (empty? springs)
+    (if (or (and (zero? run-length) (empty? sizes)) (and (= run-length (first sizes)) (= 1 (count sizes)))) 1 0)
+    (case (first springs)
+      \. (if (= run-length (first sizes))
+           (possible-arrangements (subs springs 1) (rest sizes) 0)
+           (possible-arrangements (subs springs 1) sizes 0))
+      \# (possible-arrangements (subs springs 1) sizes (inc run-length))
 
-;; TODO: (possible-arrangements ".???." [1 1] 0)
+      \? (+ (possible-arrangements (subs springs 1) sizes (inc run-length))
+            (if (= run-length (first sizes))
+              (possible-arrangements (subs springs 1) (rest sizes) 0)
+              (possible-arrangements (subs springs 1) sizes 0)))
+      )))
+
+(deftest test-possible-arrangements
+  (is (= 0 (possible-arrangements "" [] 1)))
+  (is (= 1 (possible-arrangements "#" [1] 0)))
+  (is (= 1 (possible-arrangements "##" [2] 0)))
+  (is (= 1 (possible-arrangements ".#" [1] 0)))
+  (is (= 1 (possible-arrangements "#." [1] 0)))
+  (is (= 1 (possible-arrangements "#.#" [1 1] 0)))
+  (is (= 1 (possible-arrangements "##.#" [2 1] 0)))
+  (is (= 0 (possible-arrangements "." [1] 0)))
+  (is (= 1 (possible-arrangements "?" [1] 0)))
+  (is (= 2 (possible-arrangements "??" [1] 0)))
+  (is (= 4 (possible-arrangements "??.??" [1 1] 0)))
+  (is (= 4 (possible-arrangements "????" [1] 0)))
+  (is (= 1 (possible-arrangements "???.###" [1 1 3] 0)))
+  (is (= 4 (possible-arrangements ".??..??...?##." [1 1 3] 0)))
+  (is (= 1 (possible-arrangements "?#?#?#?#?#?#?#?" [1 3 1 6] 0)))
+  (is (= 1 (possible-arrangements "????.#...#..." [4 1 1] 0)))
+  (is (= 4 (possible-arrangements "????.######..#####." [1 6 5] 0)))
+  (is (= 10 (possible-arrangements "?###????????" [3 2 1] 0)))
+)
