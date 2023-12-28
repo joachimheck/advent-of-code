a1506729d72bc899484534267196805bdb58bec0
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-12-28 06:33:27 -0700

    Finally, a solution to AoC 2023, day 12, part 2. I got an algorithm from Reddit and it still took me three days to implement it.

--- 2023/advent-12/core.clj
+++ 2023/advent-12/core.clj
@@ -67,6 +67,68 @@
 ;; "Elapsed time: 8413.2694 msecs"
 ;; 7771
 
+;; That's way too slow, and even runs out of memory, for part 2. Here's my implementation of a solution from:
+;; https://www.reddit.com/r/adventofcode/comments/18h4ign/2023_day_12_part_2_how_to_approach_part_2/
+(defn possible-arrangements
+  ([springs sizes] (possible-arrangements springs sizes 0))
+  ([springs sizes run-length]
+   (if (empty? springs)
+     (if (or (and (zero? run-length) (empty? sizes)) (and (= run-length (first sizes)) (= 1 (count sizes)))) 1 0)
+     (case (first springs)
+       \. (if (= run-length (first sizes))
+            (possible-arrangements (subs springs 1) (rest sizes) 0)
+            (if (zero? run-length)
+              (possible-arrangements (subs springs 1) sizes 0)
+              0))
+       \# (if (< run-length (or (first sizes) -1))
+            (possible-arrangements (subs springs 1) sizes (inc run-length))
+            0)
+
+       \? (+ (if (= run-length (first sizes))
+               (possible-arrangements (subs springs 1) (rest sizes) 0)
+               (if (zero? run-length)
+                 (possible-arrangements (subs springs 1) sizes 0)
+                 0))
+             (if (< run-length (or (first sizes) -1))
+               (possible-arrangements (subs springs 1) sizes (inc run-length))
+               0))))))
+
+(deftest test-possible-arrangements
+  (is (= 0 (possible-arrangements "" [] 1)))
+  (is (= 1 (possible-arrangements "#" [1])))
+  (is (= 1 (possible-arrangements "##" [2])))
+  (is (= 1 (possible-arrangements ".#" [1])))
+  (is (= 1 (possible-arrangements "#." [1])))
+  (is (= 1 (possible-arrangements "#.#" [1 1])))
+  (is (= 1 (possible-arrangements "##.#" [2 1])))
+  (is (= 0 (possible-arrangements "." [1])))
+  (is (= 1 (possible-arrangements "?" [1])))
+  (is (= 2 (possible-arrangements "??" [1])))
+  (is (= 4 (possible-arrangements "??.??" [1 1])))
+  (is (= 4 (possible-arrangements "????" [1])))
+  (is (= 1 (possible-arrangements "?.##" [2])))
+  (is (= 1 (possible-arrangements "???.###" [1 1 3])))
+  (is (= 4 (possible-arrangements ".??..??...?##." [1 1 3])))
+  (is (= 1 (possible-arrangements "?#?#?#?#?#?#?#?" [1 3 1 6])))
+  (is (= 1 (possible-arrangements "????.#...#..." [4 1 1])))
+  (is (= 4 (possible-arrangements "????.######..#####." [1 6 5])))
+  (is (= 10 (possible-arrangements "?###????????" [3 2 1]))))
+
+(defn possible-arrangement-counts [input]
+  (let [state (parse-input input)]
+    (map #(apply possible-arrangements %) state)))
+
+(defn sum-possible-arrangements [input]
+  (apply + (possible-arrangement-counts input)))
+
+;; (time (sum-possible-arrangements small-input))
+;; "Elapsed time: 4.1344 msecs"
+;; 21
+
+;; (time (sum-possible-arrangements large-input))
+;; "Elapsed time: 500.1841 msecs"
+;; 7771
+
 
 
 ;; Part 2
@@ -77,58 +139,48 @@
        (map (fn [[a b]] [(str/join "?" (repeat 5 a))
                          (load-string (str/join ["[" (str/join "," (repeat 5 b)) "]"]))]))))
 
-;; (defn arrange-recursively [springs sizes]
-;;   (str/split springs #"\.")
-;;   (loop [open-set [[springs sizes ""]]
-;;          results []]
-;;     (let [[springs-left sizes-left reconstructed :as current] (first open-set)]
-;;       (cond (empty? springs-left) (recur (rest open-set) (conj results reconstructed))
-;;             (str/starts-with? springs-left ".")
-;;             (let [[_ dots more] (re-matches #"^(\.+)(.*)" springs-left)]
-;;                (recur (assoc open-set 0 [more sizes-left (str reconstructed dots)])))
-;;             :else
-;;             :todo
-;; ))))
+;; The above implementation is still too slow by itself - gotta memoize.
+(def arrangements (atom {}))
 
-;; This idea might work in principle but I guess it will have to realize each of the
-;; possible arrangements, so it might run out of memory (or just still be slow).
+(defn possible-arrangements-memo
+  ([springs sizes] (possible-arrangements-memo springs sizes 0))
+  ([springs sizes run-length]
+   (if-let [cached (get @arrangements [springs sizes run-length])]
+     cached
+     (let [new-val (if (empty? springs)
+                     (if (or (and (zero? run-length) (empty? sizes)) (and (= run-length (first sizes)) (= 1 (count sizes)))) 1 0)
+                     (case (first springs)
+                       \. (if (= run-length (first sizes))
+                            (possible-arrangements-memo (subs springs 1) (rest sizes) 0)
+                            (if (zero? run-length)
+                              (possible-arrangements-memo (subs springs 1) sizes 0)
+                              0))
+                       \# (if (< run-length (or (first sizes) -1))
+                            (possible-arrangements-memo (subs springs 1) sizes (inc run-length))
+                            0)
 
+                       \? (+ (if (= run-length (first sizes))
+                               (possible-arrangements-memo (subs springs 1) (rest sizes) 0)
+                               (if (zero? run-length)
+                                 (possible-arrangements-memo (subs springs 1) sizes 0)
+                                 0))
+                             (if (< run-length (or (first sizes) -1))
+                               (possible-arrangements-memo (subs springs 1) sizes (inc run-length))
+                               0))))]
+       (swap! arrangements assoc [springs sizes run-length] new-val)
+       new-val))))
 
-;; Here's my implementation of a solution from:
-;; https://www.reddit.com/r/adventofcode/comments/18h4ign/2023_day_12_part_2_how_to_approach_part_2/
-(defn possible-arrangements [springs sizes run-length]
-  ;; (println "possible-arrangements" springs sizes run-length)
-  (if (empty? springs)
-    (if (or (and (zero? run-length) (empty? sizes)) (and (= run-length (first sizes)) (= 1 (count sizes)))) 1 0)
-    (case (first springs)
-      \. (if (= run-length (first sizes))
-           (possible-arrangements (subs springs 1) (rest sizes) 0)
-           (possible-arrangements (subs springs 1) sizes 0))
-      \# (possible-arrangements (subs springs 1) sizes (inc run-length))
+(defn possible-arrangement-counts-2 [input]
+  (let [state (parse-input-2 input)]
+    (map #(apply possible-arrangements-memo %) state)))
 
-      \? (+ (possible-arrangements (subs springs 1) sizes (inc run-length))
-            (if (= run-length (first sizes))
-              (possible-arrangements (subs springs 1) (rest sizes) 0)
-              (possible-arrangements (subs springs 1) sizes 0)))
-      )))
+(defn sum-possible-arrangements-2 [input]
+  (apply + (possible-arrangement-counts-2 input)))
 
-(deftest test-possible-arrangements
-  (is (= 0 (possible-arrangements "" [] 1)))
-  (is (= 1 (possible-arrangements "#" [1] 0)))
-  (is (= 1 (possible-arrangements "##" [2] 0)))
-  (is (= 1 (possible-arrangements ".#" [1] 0)))
-  (is (= 1 (possible-arrangements "#." [1] 0)))
-  (is (= 1 (possible-arrangements "#.#" [1 1] 0)))
-  (is (= 1 (possible-arrangements "##.#" [2 1] 0)))
-  (is (= 0 (possible-arrangements "." [1] 0)))
-  (is (= 1 (possible-arrangements "?" [1] 0)))
-  (is (= 2 (possible-arrangements "??" [1] 0)))
-  (is (= 4 (possible-arrangements "??.??" [1 1] 0)))
-  (is (= 4 (possible-arrangements "????" [1] 0)))
-  (is (= 1 (possible-arrangements "???.###" [1 1 3] 0)))
-  (is (= 4 (possible-arrangements ".??..??...?##." [1 1 3] 0)))
-  (is (= 1 (possible-arrangements "?#?#?#?#?#?#?#?" [1 3 1 6] 0)))
-  (is (= 1 (possible-arrangements "????.#...#..." [4 1 1] 0)))
-  (is (= 4 (possible-arrangements "????.######..#####." [1 6 5] 0)))
-  (is (= 10 (possible-arrangements "?###????????" [3 2 1] 0)))
-)
+;; (time (sum-possible-arrangements-2 small-input))
+;; "Elapsed time: 3.3463 msecs"
+;; 525152
+
+;; (time (sum-possible-arrangements-2 large-input))
+;; "Elapsed time: 2327.8278 msecs"
+;; 10861030975833
