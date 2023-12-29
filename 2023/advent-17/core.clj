c1492291b6dd8db625f81da27464d4d2ab25f411
joachimheck <51496639+joachimheck@users.noreply.github.com>
2023-12-29 12:07:57 -0700

    Solution to AoC 2023, day 17, part 1.

--- 2023/advent-17/core.clj
+++ 2023/advent-17/core.clj
@@ -74,51 +74,75 @@
     (if (some #{current} (keys came-from))
       (recur (get came-from current)
              (conj path current))
-      path)))
-
-(defn find-neighbors [[x y :as p] came-from]
-  (let [dir (if (get came-from p)
-              (direction (get came-from p) p)
-              :right)
-        three-back (get came-from (get came-from (get came-from p)))
-        inaccessible #{(move x y (opposite dir) 1)
-                       (if (= three-back (move x y (opposite dir) 3))
-                         (move x y dir 1))}]
-    (remove inaccessible (adjacent p))))
+      (map :point path))))
 
 (defn in-bounds [points width height]
   (filter (fn [[x y]] (and (< -1 x width) (< -1 y height))) points))
 
+(defn find-neighbors [{:keys [point prevs] :as p} came-from width height]
+  ;; (println "find-neighbors" p came-from)
+  (let [[x y] point
+        ;; _ (if (or (nil? x) (nil? y)) (println "nil x or y" p))
+        ;; _ (println "find-neighbors point" point "new prevs" prevs)
+        dir (if (first prevs)
+              (direction (first prevs) point)
+              :right)
+        prevs (conj prevs point)
+        inaccessible #{(move x y (opposite dir) 1)
+                       (if (or (apply = (map first prevs)) (apply = (map second prevs)))
+                         (move x y dir 1))}]
+    ;; (println "adjacent" (adjacent point) "dir" dir "inaccessible" inaccessible)
+    ;; (println "find-neighbors new prevs" (take 3 (conj prevs point)))
+    ;; (println "find-neighbors neighbors" (in-bounds (remove inaccessible (adjacent point)) width height))
+    (map (fn [p] {:point p :prevs (take 3 prevs)}) (in-bounds (remove inaccessible (adjacent point)) width height))))
+
+
+  ;; (let [dir (if (get came-from p)
+  ;;             (direction (get came-from p) p)
+  ;;             :right)
+  ;;       three-back (get came-from (get came-from (get came-from p)))
+  ;;       inaccessible #{(move x y (opposite dir) 1)
+  ;;                      (if (= three-back (move x y (opposite dir) 3))
+  ;;                        (move x y dir 1))}]
+  ;;   (remove inaccessible (adjacent p)))
+
 (defn manhattan-distance [[x y :as p] [goalx goaly]]
   (+ (abs (- x goalx)) (abs (- y goaly))))
 
+(defn point-with-lowest-f-score [points f-scores]
+  (->> points
+       (map #(list (:point %) (get f-scores % Integer/MAX_VALUE) %))
+       (sort-by first)
+       (sort-by second)
+       (first)
+       (last))
+  ;;(first (first (sort-by second (sort-by first (map #(list % (get f-scores % Integer/MAX_VALUE)) points)))))
+  )
+
 (defn find-optimal-path [{:keys [width height grid]} h-fn]
-  (let [start [0 0]
+  (let [start {:point [0 0] :prevs (list nil nil nil)}
         goal [(dec width) (dec height)]]
     (loop [open-set #{start}
            came-from {}
            g-scores {start 0}
-           f-scores {start (h-fn start goal)}
+           f-scores {start (h-fn (:point start) goal)}
            i 0]
       ;; (println "open-set" open-set)
       (if (empty? open-set)
         :failure-no-path
-        (let [current (first (first (sort-by second (sort-by first (map #(list % (get f-scores % Integer/MAX_VALUE)) open-set)))))]
-          (if (= current goal)
+        (let [current (point-with-lowest-f-score open-set f-scores)]
+          (if (= (:point current) goal)
             (reconstruct-path came-from current)
-            (let [neighbors (in-bounds (find-neighbors current came-from) width height)
-                  ;; _ (println "neighbors of" current (if (get came-from current)
-                  ;;                                     (direction (get came-from current) current)
-                  ;;                                     :no-direction)
-                  ;;            neighbors)
+            (let [neighbors (find-neighbors current came-from width height)
+                  ;; _ (println "neighbors of" current neighbors)
                   [new-open-set new-came-from new-g-scores new-f-scores]
                   (reduce (fn [[open-set came-from g-scores f-scores :as acc] n]
-                            (let [tentative-g-score (+ (get g-scores current) (get grid n))]
+                            (let [tentative-g-score (+ (get g-scores current) (get grid (:point n)))]
                               (if (< tentative-g-score (get g-scores n Integer/MAX_VALUE))
                                 [(conj open-set n)
                                  (assoc came-from n current)
                                  (assoc g-scores n tentative-g-score)
-                                 (assoc f-scores n (+ tentative-g-score (h-fn n goal)))]
+                                 (assoc f-scores n (+ tentative-g-score (h-fn (:point n) goal)))]
                                 acc)))
                           [open-set came-from g-scores f-scores]
                           neighbors)]
@@ -131,6 +155,7 @@
 (defn min-path-heat-loss [input]
   (let [{:keys [width height grid] :as pattern} (parse-input input)
         path (find-optimal-path pattern manhattan-distance)]
+    ;; (println "path" path)
     (apply + (map #(get grid %) path))))
 
 (defn draw-path [input]
@@ -146,5 +171,10 @@
                                (map list path (concat [[0 0]] path)))]
     (println (pattern-to-string (assoc pattern :grid grid-with-path)))))
 
-;; ---> answer <---
-;; 977
+;; (time (min-path-heat-loss small-input))
+;; "Elapsed time: 2506.7858 msecs"
+;; 102
+
+;; (time (min-path-heat-loss large-input))
+;; "Elapsed time: 1.47727566268E7 msecs"
+;; 928
