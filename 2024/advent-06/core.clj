e15116159f7cc3b92fef9b85bb982e2eaebe9958
joachimheck <51496639+joachimheck@users.noreply.github.com>
2024-12-09 16:15:37 -0700

    Solution to Advent of Code 2024 day 6, part 2.
    I couldn't think of an optimized solution so I took a fifteen minute walk and it was done when I got home.

--- 2024/advent-06/core.clj
+++ 2024/advent-06/core.clj
@@ -36,9 +36,6 @@
 ;; Predict the path of the guard and count the positions she reaches.
 (def dirs [:east :south :west :north])
 
-(defn turn-right [dir]
-  (first (drop 1 (drop-while #(not= % dir) (take 5 (cycle dirs))))))
-
 (defn move-dir [grid [i j dir]]
   (case dir
     :east [(inc i) j dir]
@@ -77,25 +74,62 @@
 
 ;; Part 2
 ;; Where can an object be placed to make the guard's path a loop?
-(defn loop? [{:keys [grid height width start]}]
-  (loop [pos start
+(def move-fns {:east (fn [[i j d]] [(inc i) j d])
+               :south (fn [[i j d]] [i (inc j) d])
+               :west (fn [[i j d]] [(dec i) j d])
+               :north (fn [[i j d]] [i (dec j) d])
+               })
+
+(defn move-forward
+  "Returns the positions the guard will move through before encountering an obstacle or leaving the room."
+  [{:keys [grid width height]} start]
+  (let [[x y dir] start
+        move-fn (dir move-fns)]
+    (loop [pos [x y dir] posns []]
+      (let [[i j d :as newpos] (move-fn pos)]
+        (cond (= \# (get-in grid [j i]))
+              posns
+              (or (not (< -1 i width))
+                  (not (< -1 j height)))
+              (conj posns newpos)
+              :else
+              (recur newpos (conj posns newpos)))))))
+
+(defn turn-right [dir]
+  ;; (first (drop 1 (drop-while #(not= % dir) (take 5 (cycle dirs)))))
+  (get dirs (mod (inc (.indexOf dirs dir)) 4)))
+
+(defn loop? [{:keys [grid width height] :as state} start]
+  (loop [[i j d :as pos] start
          positions [start]]
-    (let [[ni nj nd :as next] (move grid pos)]
+    (let [newposns (move-forward state pos)
+          [ni nj nd :as next] (last newposns)]
       (cond
-        (some #{next} positions)
-        (is-in? positions next)
-        true
+        (empty? newposns)
+        (recur [i j (turn-right d)] positions)
+        (some (set newposns) positions)
+        [true (concat positions newposns)]
         (and (< -1 ni width) (< -1 nj height))
-        (recur next (conj positions next))
+        (recur next (concat positions newposns))
         :else
-        false))))
+        [false (concat positions newposns)]))))
 
 (defn count-loop-positions [input]
   (let [{:keys [grid height width start] :as state} (parse-input input)]
     (count
-      (filter true?
-              (for [j (range height)
-                    i (range width)
-                    :when (not= [i j] (take 2 start))
-                    :let [new-state (assoc state :grid (assoc-in grid (reverse [i j]) \#))]]
-                (loop? new-state))))))
+     (filter #(true? (first %))
+             (for [j (range height)
+                   ;; :let [_ (println "Processing row" j)]
+                   i (range width)
+                   :when (not= [i j] (take 2 start))
+                   :let [new-state (assoc state :grid (assoc-in grid (reverse [i j]) \#))
+                         ;; _ (Thread/sleep 10)
+                         ]]
+               (loop? new-state start))))))
+
+;; (time (count-loop-positions small-input))
+;; "Elapsed time: 18.9686 msecs"
+;; 6
+;; (time (count-loop-positions large-input))
+;; "Elapsed time: 635175.0989 msecs"
+;; 1705
