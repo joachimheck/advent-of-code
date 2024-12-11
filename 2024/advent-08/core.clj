7c9b3c4b637f82ed82d34ccd5a7ca0ad69cb14e5
joachimheck <51496639+joachimheck@users.noreply.github.com>
2024-12-11 10:20:51 -0700

    Solution to AoC 2024 day 8, part 2.

--- 2024/advent-08/core.clj
+++ 2024/advent-08/core.clj
@@ -61,3 +61,36 @@
 ;; (time (count-antinode-locations large-input))
 ;; "Elapsed time: 7.7905 msecs"
 ;; 359
+
+
+;; Part 2
+;; Antinodes can appear at any distance from the antennae.
+(defn count-antinode-locations-2 [input]
+  (let [the-map (parse-input input)
+        by-id (reduce (fn [acc [k v]]
+                        (assoc acc v (conj (get acc v '()) k)))
+                      {}
+                      (dissoc the-map :width :height))
+        antenna-pairs (apply concat
+                             (for [id (keys by-id)
+                                   :when (> (count (get by-id id)) 1)]
+                               (pairs (get by-id id))))
+        all-antinodes (apply concat
+                             (for [[[x1 y1] [x2 y2]] antenna-pairs
+                                   :let [xdiff (- x2 x1)
+                                         ydiff (- y2 y1)]]
+                               (loop [[x1 y1 :as p1] [x1 y1] [x2 y2 :as p2] [x2 y2] antinodes []]
+                                 (if (or (in-bounds p1 (:width the-map) (:height the-map))
+                                         (in-bounds p2 (:width the-map) (:height the-map)))
+                                   (let [new-p1 [(- x1 xdiff) (- y1 ydiff)]
+                                         new-p2 [(+ x2 xdiff) (+ y2 ydiff)]]
+                                     (recur new-p1 new-p2 (conj antinodes p1 p2)))
+                                   antinodes))))]
+    (count (distinct (filter #(in-bounds % (:width the-map) (:height the-map)) all-antinodes)))))
+
+;; (time (count-antinode-locations-2 small-input))
+;; "Elapsed time: 2.6869 msecs"
+;; 34
+;; (time (count-antinode-locations-2 large-input))
+;; "Elapsed time: 20.4975 msecs"
+;; 1293
