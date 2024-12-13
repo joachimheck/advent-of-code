56ec41dd4366e3915beea98058fe865e784df52b
joachimheck <51496639+joachimheck@users.noreply.github.com>
2024-12-13 11:55:20 -0700

    Solution to AoC 2024 day 11, part 2.

--- 2024/advent-11/core.clj
+++ 2024/advent-11/core.clj
@@ -26,17 +26,19 @@
 
 ;; Part 1
 ;; How many stones will there be after 25 blinks?
-(defn blink [stones]
+(defn blink [stone]
+  (cond (= stone "0")
+        ["1"]
+        (even? (count stone))
+        [(subs stone 0 (quot (count stone) 2))
+         (str (parse-long (subs stone (quot (count stone) 2))))]
+        :else
+        [(str (* 2024 (parse-long stone)))]))
+
+(defn blink-multiple [stones]
   (apply concat
          (for [stone stones]
-           (cond (= stone "0")
-                 ["1"]
-                 (even? (count stone))
-                 [(subs stone 0 (quot (count stone) 2))
-                  (str (parse-long (subs stone (quot (count stone) 2))))]
-                 :else
-                 [(str (* 2024 (parse-long stone)))]
-                 ))))
+           (blink stone))))f
 
 (defn blink-multiple-count [input n]
   (count
@@ -44,12 +46,11 @@
     (loop [stones (parse-input input)
            i n]
       (let [_ (Thread/sleep 1)
-            result (blink stones)
+            result (blink-multiple stones)
             _ (println "blink" (inc (- n i)) (count result))]
        (if (= i 0)
          stones
          (recur result (dec i))))))))
-
 
 ;; (time (blink-multiple-count small-input 25))
 ;; "Elapsed time: 55.5027 msecs"
@@ -61,5 +62,44 @@
 
 ;; Part 2
 ;; How many stones will there be after 75 blinks?
+(defn blink-build-tree [stones n]
+  (loop [productions {}
+         stones stones
+         i 0]
+    (if (= i n)
+      productions
+      (let [keyset (set (keys productions))
+            new-stones (get (group-by keyset stones) nil)
+            new-prods (map #(vector % (blink %)) new-stones)]
+        (recur (into productions new-prods)
+               (remove keyset (apply concat (map second new-prods)))
+               (inc i))))))
 
-;; Try computing each number's evolution separately. Note how 2024 evolves in the example.
+(defn blink-walk-tree [tree stones n]
+  (loop [stone-freqs (frequencies stones)
+         i 0]
+    (let [new-freqs (apply concat (map (fn [[s f]] (map #(vector % f) (get tree s))) stone-freqs))
+          condensed-freqs (into {} (map (fn [[k vs]] [k (apply + (map second vs))]) (group-by first new-freqs)))]
+      (if (= i n)
+        (apply + (map second stone-freqs))
+        (recur condensed-freqs
+               (inc i))))))
+
+(defn blink-count-with-tree [input n]
+  (let [stones (parse-input input)
+        tree (blink-build-tree stones 75)]
+    (blink-walk-tree tree stones n)))
+
+
+;; (time (blink-count-with-tree small-input 25))
+;; "Elapsed time: 7.494 msecs"
+;; 55312
+;; (time (blink-count-with-tree large-input 25))
+;; "Elapsed time: 68.3795 msecs"
+;; 193899
+;; (time (blink-count-with-tree small-input 75))
+;; "Elapsed time: 14.7395 msecs"
+;; 65601038650482
+;; (time (blink-count-with-tree large-input 75))
+;; "Elapsed time: 436.2177 msecs"
+;; 229682160383225
