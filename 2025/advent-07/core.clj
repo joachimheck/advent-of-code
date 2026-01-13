26bb185b83f23d82ff6af5de01aba5a28046fb9a
joachimheck <51496639+joachimheck@users.noreply.github.com>
2026-01-12 18:08:20 -0700

    Solution to AoC 2025, day 7, part 2.
    I had to work out the example problem by hand to find the bug. The idea was correct.

--- 2025/advent-07/core.clj
+++ 2025/advent-07/core.clj
@@ -1,4 +1,4 @@
-(ns advent-07.core)
+ (ns advent-07.core)
 
 (require '[clojure.pprint :as pp])
 (require '[clojure.repl :refer :all])
@@ -76,42 +76,55 @@
 
 ;; Part 2
 ;; How many paths are there through the beam splitters?
-(defn trace-beam-paths [{:keys [grid bounds start] :as state}]
-  (loop [currents {start 1}
-         n 0]
-    ;; (println "first currents" (first currents)) (flush)
-    (if (>= n (dec (second bounds)))
-      currents
-      (let [nexts (reduce (fn [nexts [[x y] path-count]]
-                            ;; (println "nexts" nexts)
-                            (if (= \^ (get grid [x (inc y)]))
-                              (let [u1 (update nexts [(dec x) (inc y)] (fn [v] (if (nil? v) path-count (+ v path-count))))
-                                    u2 (update u1 [(inc x) (inc y)] (fn [v] (if (nil? v) path-count (+ v path-count))))
-                                    _ (println "xy" [x y] "nexts" nexts "u1" u1 "u2" u2)
-                                    ;; _ (flush)
-                                    ]
-                                ;; (update
-                               ;; (update nexts [(dec x) (inc y)] (fn [v] (if (nil? v) 1 (inc v))))
-                               ;; [(inc x) (inc y)] (fn [v] (if (nil? v) 1 (inc v))))
-                                u2)
-                              (let [u3 (assoc nexts [x (inc y)] path-count)
-                                    ;; _ (println "u3" u3 "x" x "y" y)
-                                    ]
-                                u3)))
-                          {}
-                          currents)]
-        (recur nexts (inc n))))))
+(defn trace-beam-paths
+  ([{:keys [grid bounds start] :as state}]
+   (trace-beam-paths state (dec (second bounds))))
+  ([{:keys [grid bounds start] :as state} max-y]
+   (loop [currents {start 1}
+          n 0]
+     ;; (println "currents" currents)
+     (if (>= n max-y)
+       currents
+       (let [nexts (reduce (fn [nexts [[x y] path-count]]
+                             ;; (println "nexts" nexts)
+                             (if (= \^ (get grid [x (inc y)]))
+                               (let [u1 (update nexts [(dec x) (inc y)] (fn [v] (+ (if (nil? v) 0 v) path-count)))
+                                     u2 (update u1 [(inc x) (inc y)] (fn [v] (+ (if (nil? v) 0 v) path-count)))
+                                     ;; _ (println "xy" [x y] "nexts" nexts "u2" u2)
+                                     ;; _ (flush)
+                                     ]
+                                 ;; (update
+                                 ;; (update nexts [(dec x) (inc y)] (fn [v] (if (nil? v) 1 (inc v))))
+                                 ;; [(inc x) (inc y)] (fn [v] (if (nil? v) 1 (inc v))))
+                                 u2)
+                               (let [u3 (update nexts [x (inc y)] (fn [v] (+ (if (nil? v) 0 v) path-count)))
+                                     ;; _ (println "xy" [x y] "u3" u3)
+                                     ]
+                                 u3)))
+                           {}
+                           currents)]
+         (recur nexts (inc n)))))))
 
-(defn count-beam-paths [{:keys [grid bounds start] :as state}]
-  (apply + (map second (filter #(= (dec (second bounds)) (second (first %))) (trace-beam-paths state)))))
+(defn count-beam-paths
+  ([{:keys [grid bounds start] :as state} max-y]
+   (apply + (map second (filter #(= max-y (second (first %))) (trace-beam-paths state max-y)))))
+  ([{:keys [grid bounds start] :as state}]
+   (apply + (map second (filter #(= (dec (second bounds)) (second (first %))) (trace-beam-paths state)))))
+  )
 
 (def test-input-1 ["...S..." "...^..." "......."])
 (def test-input-2 ["...S..." "...^..." "..^.^.." "......."])
+(def test-input-3 ["...S..." "...^..." "..^.^.." ".^.^.^." "......."])
+(def test-input-4 ["....S...." "....^...." "...^.^..." "..^.^.^.." ".^.^...^." "........."])
 
 (deftest test-inputs
-  (is (= 2 (count-beam-paths (handle-input test-input-1))))
-  (is (= 4 (count-beam-paths (handle-input test-input-2)))))
-
+  (is (= 2 (count-beam-paths (parse-input small-input) 2)))
+  (is (= 4 (count-beam-paths (parse-input small-input) 4)))
+  (is (= 8 (count-beam-paths (parse-input small-input) 6)))
+  (is (= 13 (count-beam-paths (parse-input small-input) 8)))
+  (is (= 20 (count-beam-paths (parse-input small-input) 10)))
+  (is (= 26 (count-beam-paths (parse-input small-input) 12)))
+  (is (= 40 (count-beam-paths (parse-input small-input)))))
 ;; ...S...
 ;; ...^...
 ;; .......
@@ -120,3 +133,24 @@
 ;; ...^...
 ;; ..^.^..
 ;; .......
+
+;; ...S...
+;; ...^...
+;; ..^.^..
+;; .^.^.^.
+;; .......
+
+;; ....S....
+;; ....^....
+;; ...^.^...
+;; ..^.^.^..
+;; .^.^...^.
+;; .........
+
+
+;; (time (count-beam-paths (parse-input small-input)))
+;; "Elapsed time: 2.2263 msecs"
+;; 40
+;; (time (count-beam-paths (parse-input large-input)))
+;; "Elapsed time: 53.5705 msecs"
+;; 20571740188555
