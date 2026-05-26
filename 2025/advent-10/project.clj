202d08b86f917c7add224c605ae24dfe0c2a018d
joachimheck <51496639+joachimheck@users.noreply.github.com>
2026-05-26 08:56:52 -0600

    Work on AoC 2025, day 10

--- 2025/advent-10/project.clj
+++ 2025/advent-10/project.clj
@@ -6,6 +6,7 @@
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [org.ojalgo/ojalgo "56.1.1"]
                  [net.mikera/core.matrix "0.63.0"]
+                 ;; [uncomplicate/neanderthal "0.62.0"]
                  [io.github.tonsky/vectorz-clj "0.48.1"]]
   :repl-options {:init-ns advent-10.core}
   :jvm-opts ["-Xmx1G" "-XX:-OmitStackTraceInFastThrow"])
